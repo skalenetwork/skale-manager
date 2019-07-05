4875681e30c0a1aa4d4273c9070a4dd6936ba1ea
Artem Payvin <payvinartem@gmail.com>
2019-07-05 17:50:53 +0300

    SKALE-1261 update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -68,7 +68,7 @@ module.exports = {
                   enabled: true,
                   runs: 200
                 },
-                evmVersion: "byzantium"
+                evmVersion: "petersburg"
               }
         }
       }
