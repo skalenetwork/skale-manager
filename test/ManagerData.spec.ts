7d7f395fe0eef5a441f7636a578499129c5b8741
elvisgiv <alexeymaratovich@gmail.com>
2019-07-10 11:08:45 +0300

    SKALE-1292 Writed unit tests for ManagerData.sol

--- test/ManagerData.spec.ts
+++ test/ManagerData.spec.ts
@@ -1,157 +1,59 @@
-import {ConstantsHolderContract,
-        ConstantsHolderInstance,
+import {
         ContractManagerContract,
-        ContractManagerInstance} from "../types/truffle-contracts";
+        ContractManagerInstance,
+        ManagerDataContract,
+        ManagerDataInstance
+      } from "../types/truffle-contracts";
 
 import chai = require("chai");
 import * as chaiAsPromised from "chai-as-promised";
 import {BigNumber} from 'bignumber.js'
 import { skipTime } from "./utils/time";
-
 chai.should();
-chai.use((chaiAsPromised as any));
-
-const moment = require('moment');
-
+chai.use((chaiAsPromised));
 
 const ContractManager: ContractManagerContract = artifacts.require("./ContractManager");
-const ConstantsHolder: ConstantsHolderContract = artifacts.require("./ConstantsHolder");
+const ManagerData: ManagerDataContract = artifacts.require("./ManagerData");
 
 contract("ContractManager", ([deployer, user]) => {
   let contractManager: ContractManagerInstance;
-  let constantsHolder: ConstantsHolderInstance;
+  let managerData: ManagerDataInstance;
 
   before(async () => {
     contractManager = await ContractManager.new({from: deployer});
-    constantsHolder = await ConstantsHolder.new(contractManager.address, {from: deployer});
+    managerData = await ManagerData.new("SkaleManager", contractManager.address, {from: deployer});
   });
 
-  it("NODE_DEPOSIT should be equal 100000000000000000000", async () => {
-    let bn = new BigNumber(await constantsHolder.NODE_DEPOSIT())
-    // convert Big Number to string and then string to number and compare with
-    parseInt(bn.toString(), 10).should.be.equal(100000000000000000000)
-    
-  });
-
-  it("TINY_DIVISOR should be equal 128", async () => {
-    let bn = new BigNumber(await constantsHolder.TINY_DIVISOR())
-    parseInt(bn.toString(), 10).should.be.equal(128)
-  });
-
-  it("SMALL_DIVISOR should be equal 8", async () => {
-    let bn = new BigNumber(await constantsHolder.SMALL_DIVISOR())
-    parseInt(bn.toString(), 10).should.be.equal(8)
-  });
-
-  it("MEDIUM_DIVISOR should be equal 1", async () => {
-    let bn = new BigNumber(await constantsHolder.MEDIUM_DIVISOR())
-    parseInt(bn.toString(), 10).should.be.equal(1)
-  });
-
-  it("MEDIUM_TEST_DIVISOR should be equal 4", async () => {
-    let bn = new BigNumber(await constantsHolder.MEDIUM_TEST_DIVISOR())
-    parseInt(bn.toString(), 10).should.be.equal(4)
-  });
-
-  it("NUMBER_OF_NODES_FOR_SCHAIN should be equal 16", async () => {
-    let bn = new BigNumber(await constantsHolder.NUMBER_OF_NODES_FOR_SCHAIN())
-    parseInt(bn.toString(), 10).should.be.equal(16)
-  });
-
-  it("NUMBER_OF_NODES_FOR_TEST_SCHAIN should be equal 2", async () => {
-    let bn = new BigNumber(await constantsHolder.NUMBER_OF_NODES_FOR_TEST_SCHAIN())
-    parseInt(bn.toString(), 10).should.be.equal(2)
-  });
-
-  it("NUMBER_OF_NODES_FOR_MEDIUM_TEST_SCHAIN should be equal 4", async () => {
-    let bn = new BigNumber(await constantsHolder.NUMBER_OF_NODES_FOR_MEDIUM_TEST_SCHAIN())
-    parseInt(bn.toString(), 10).should.be.equal(4)
-  });
-
-  it("FRACTIONAL_FACTOR should be equal 128", async () => {
-    let bn = new BigNumber(await constantsHolder.FRACTIONAL_FACTOR())
-    parseInt(bn.toString(), 10).should.be.equal(128)
-  });
-
-  it("FULL_FACTOR should be equal 17", async () => {
-    let bn = new BigNumber(await constantsHolder.FULL_FACTOR())
-    parseInt(bn.toString(), 10).should.be.equal(17)
-  });
-
-  it("SECONDS_TO_DAY should be equal 86400", async () => {
-    let bn = new BigNumber(await constantsHolder.SECONDS_TO_DAY())
-    parseInt(bn.toString(), 10).should.be.equal(86400)
-  });
-
-  it("SECONDS_TO_MONTH should be equal 2592000", async () => {
-    let bn = new BigNumber(await constantsHolder.SECONDS_TO_MONTH())
-    parseInt(bn.toString(), 10).should.be.equal(2592000)
-  });
-
-  it("SECONDS_TO_YEAR should be equal 31622400", async () => {
-    let bn = new BigNumber(await constantsHolder.SECONDS_TO_YEAR())
-    parseInt(bn.toString(), 10).should.be.equal(31622400)
-  });
-
-  it("SIX_YEARS (in seconds) should be equal 186624000", async () => {
-    let bn = new BigNumber(await constantsHolder.SIX_YEARS())
-    parseInt(bn.toString(), 10).should.be.equal(186624000)
-  });
-
-  it("NUMBER_OF_VALIDATORS should be equal 21", async () => {
-    let bn = new BigNumber(await constantsHolder.NUMBER_OF_VALIDATORS())
-    parseInt(bn.toString(), 10).should.be.equal(21)
-  });
-
-  it("rewardPeriod should be equal 600", async () => {
-    let bn = new BigNumber(await constantsHolder.rewardPeriod())
-    parseInt(bn.toString(), 10).should.be.equal(600)
-  });
-
-  it("deltaPeriod should be equal 240", async () => {
-    let bn = new BigNumber(await constantsHolder.deltaPeriod())
-    parseInt(bn.toString(), 10).should.be.equal(240)
-  });
-
-  it("lastTimeUnderloaded should be equal 0", async () => {
-    let bn = new BigNumber(await constantsHolder.lastTimeUnderloaded())
+  it("minersCap should be equal 0", async () => {
+    let bn = new BigNumber(await managerData.minersCap())
     parseInt(bn.toString(), 10).should.be.equal(0)
   });
 
-  it("lastTimeOverloaded should be equal 0", async () => {
-    let bn = new BigNumber(await constantsHolder.lastTimeOverloaded())
+  it("startTime should be equal 0", async () => {
+    let bn = new BigNumber(await managerData.startTime())
+    parseInt(bn.toString(), 10).should.not.equal(0)
+  });
+
+  it("stageTime should be equal 0", async () => {
+    let bn = new BigNumber(await managerData.stageTime())
     parseInt(bn.toString(), 10).should.be.equal(0)
   });
 
-  it("should invoke setPeriods function and change rewardPeriod and deltaPeriod", async () => {
-    await constantsHolder.setPeriods(333, 555, {from: deployer})
-    let rewardPeriod = new BigNumber(await constantsHolder.rewardPeriod())
-    parseInt(rewardPeriod.toString(), 10).should.be.equal(333)
-
-    let deltaPeriod = new BigNumber(await constantsHolder.deltaPeriod())
-    parseInt(deltaPeriod.toString(), 10).should.be.equal(555)
+  it("stageNodes should be equal 0", async () => {
+    let bn = new BigNumber(await managerData.stageNodes())
+    parseInt(bn.toString(), 10).should.be.equal(0)
   });
 
-  it("should Set time if system underloaded", async () => {
-    let sec = 10  
-    await constantsHolder.setLastTimeUnderloaded({from: deployer})
-    let bn = new BigNumber(await constantsHolder.lastTimeUnderloaded())
-    skipTime(web3, sec);
-    await constantsHolder.setLastTimeUnderloaded({from: deployer})
-    let btn = new BigNumber(await constantsHolder.lastTimeUnderloaded())
-    // parseInt(bn.toString(), 10).should.be.equal(0)
-    expect(parseInt(btn.toString(), 10) - parseInt(bn.toString(), 10)).to.equal(sec)
+  it("should sets miners capitalization", async () => {
+    await managerData.setMinersCap(333, {from: deployer})
+    let minersCap = new BigNumber(await managerData.minersCap())
+    parseInt(minersCap.toString(), 10).should.be.equal(333)
   });
 
-  it("should Set time if system iverloaded", async () => {
-    let sec = 10  
-    await constantsHolder.setLastTimeOverloaded({from: deployer})
-    let bn = new BigNumber(await constantsHolder.lastTimeOverloaded())
-    skipTime(web3, sec);
-    await constantsHolder.setLastTimeOverloaded({from: deployer})
-    let btn = new BigNumber(await constantsHolder.lastTimeOverloaded())
-    // parseInt(bn.toString(), 10).should.be.equal(0)
-    expect(parseInt(btn.toString(), 10) - parseInt(bn.toString(), 10)).to.equal(sec)
+  it("should sets new stage time and new amount of Nodes at this stage", async () => {
+    await managerData.setStageTimeAndStageNodes(120, {from: deployer})
+    let minersCap = new BigNumber(await managerData.stageNodes())
+    parseInt(minersCap.toString(), 10).should.be.equal(120)
   });
- 
+
 });
