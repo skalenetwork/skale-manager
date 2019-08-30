a4d4ca0c729a4ecb6c83ae04edf4799b0d177515
Dmytro Stebaiev <dmytro@skalelabs.com>
2019-08-30 12:58:13 +0300

    SKALE-1367 Fix memory error

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {    
     compileCommand: '../node_modules/.bin/truffle compile --network coverage',
-    testCommand: '../node_modules/.bin/truffle test --network coverage --gas_multiplier 10',    
+    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage --gas_multiplier 10',
     norpc: true,
     skipFiles: ['Constants.sol']
 };
