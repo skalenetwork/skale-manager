8eb7f07ce4a566a4af00f61ccc7da865b046b595
Dmytro Stebaiev <dmytro@skalelabs.com>
2019-09-12 16:14:22 +0300

    SKALE-1367 Increase node memory limit

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {    
     compileCommand: '../node_modules/.bin/truffle compile --network coverage',
-    testCommand: 'node --max-old-space-size=8192 ../node_modules/.bin/truffle test --network coverage --gas_multiplier 10',
+    testCommand: '../node_modules/.bin/truffle test --network coverage --gas_multiplier 10',
     norpc: true,
     skipFiles: ['Constants.sol']
 };
