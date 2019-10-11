54cab81e6e15f02b4f04278f21af4d39e94cf002
Artem Payvin <payvinartem@gmail.com>
2019-10-11 14:21:41 +0300

    Added test for closing channel

--- test/SkaleDKG.ts
+++ test/SkaleDKG.ts
@@ -249,6 +249,27 @@ contract("SkaleDKG", ([validator1, validator2]) => {
             assert(channel.active.should.be.true);
         });
 
+        it("should create & delete schain and open & close a DKG channel", async () => {
+            const deposit = await schainsFunctionality.getSchainPrice(4, 5);
+
+            await schainsFunctionality.addSchain(
+                validator1,
+                deposit,
+                "0x10" +
+                "0000000000000000000000000000000000000000000000000000000000000005" +
+                "04" +
+                "0000" +
+                "6432",
+                {from: validator1});
+
+            let channel: Channel = new Channel(await skaleDKG.channels(web3.utils.soliditySha3("d2")));
+            assert(channel.active.should.be.true);
+
+            await schainsFunctionality.deleteSchainByRoot("d2", {from: validator1});
+            channel = new Channel(await skaleDKG.channels(web3.utils.soliditySha3("d2")));
+            assert(channel.active.should.be.false);
+        });
+
         describe("when 2-node schain is created", async () => {
             beforeEach(async () => {
                 const deposit = await schainsFunctionality.getSchainPrice(4, 5);
