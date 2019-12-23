7b8299f21a55de7afc58eb28f6fd67994900ed11
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-12-23 13:00:29 +0200

    SKALE-1897 Add Docker container build, add releases to the Travis file

---
+++ scripts/build_and_publish.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+: "${VERSION?Need to set VERSION}"
+: "${BRANCH?Need to set BRANCH}"
+
+NAME=skale-manager
+REPO_NAME=skalelabshub/$NAME
+IMAGE_NAME=$REPO_NAME:$VERSION
+LATEST_IMAGE_NAME=$REPO_NAME:$BRANCH-latest
+
+# Build image
+
+echo "Building $IMAGE_NAME..."
+docker build -t $IMAGE_NAME . || exit $?
+docker tag $IMAGE_NAME $LATEST_IMAGE_NAME
+
+echo "========================================================================================="
+echo "Built $IMAGE_NAME"
+
+# Publish image
+
+: "${USERNAME?Need to set USERNAME}"
+: "${PASSWORD?Need to set PASSWORD}"
+
+echo "$PASSWORD" | docker login --username $USERNAME --password-stdin
+
+docker push $IMAGE_NAME || exit $?
+docker push $LATEST_IMAGE_NAME || exit $?
