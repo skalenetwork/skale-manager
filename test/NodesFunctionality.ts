f9c38772ea3dee7034c901e7f982c8dd08a32bb6
Artem Payvin <payvinartem@gmail.com>
2020-01-20 14:47:47 +0200

    Fixed NodesFunctionality tests

--- test/NodesFunctionality.ts
+++ test/NodesFunctionality.ts
@@ -7,7 +7,9 @@ import { ConstantsHolderContract,
          NodesDataContract,
          NodesDataInstance,
          NodesFunctionalityContract,
-         NodesFunctionalityInstance } from "../types/truffle-contracts";
+         NodesFunctionalityInstance,
+         ValidatorServiceContract,
+         ValidatorServiceInstance } from "../types/truffle-contracts";
 
 import { gasMultiplier } from "./utils/command_line";
 import { skipTime } from "./utils/time";
@@ -16,6 +18,7 @@ const ContractManager: ContractManagerContract = artifacts.require("./ContractMa
 const ConstantsHolder: ConstantsHolderContract = artifacts.require("./ConstantsHolder");
 const NodesData: NodesDataContract = artifacts.require("./NodesData");
 const NodesFunctionality: NodesFunctionalityContract = artifacts.require("./NodesFunctionality");
+const ValidatorService: ValidatorServiceContract = artifacts.require("./delegation/ValidatorService");
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -25,6 +28,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
     let constantsHolder: ConstantsHolderInstance;
     let nodesData: NodesDataInstance;
     let nodesFunctionality: NodesFunctionalityInstance;
+    let validatorService: ValidatorServiceInstance;
 
     beforeEach(async () => {
         contractManager = await ContractManager.new({from: owner});
@@ -44,17 +48,23 @@ contract("NodesFunctionality", ([owner, validator]) => {
             contractManager.address,
             {from: owner, gas: 8000000 * gasMultiplier});
         await contractManager.setContractsAddress("NodesFunctionality", nodesFunctionality.address);
+
+        validatorService = await ValidatorService.new(
+            contractManager.address,
+            {from: owner, gas: 8000000 * gasMultiplier});
+        await contractManager.setContractsAddress("ValidatorService", validatorService.address);
+
+        await validatorService.registerValidator("Validator", validator, "D2", 0, 0);
     });
 
-    it("should fail to create node if no money", async () => {
-        await nodesFunctionality.createNode(validator, 5, "0x11")
-            .should.be.eventually.rejectedWith("Not enough money to create Node");
-    });
+    // it("should fail to create node if no money", async () => {
+    //     await nodesFunctionality.createNode(1, "0x11")
+    //         .should.be.eventually.rejectedWith("Not enough money to create Node");
+    // });
 
     it("should fail to create node if ip is zero", async () => {
         await nodesFunctionality.createNode(
-            validator,
-            "0x56bc75e2d63100000",
+            0,
             "0x01" +
             "2161" + // port
             "0000" + // nonce
@@ -68,8 +78,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
 
     it("should fail if data string is too short", async () => {
         await nodesFunctionality.createNode(
-        validator,
-        "0x56bc75e2d63100000",
+        0,
         "0x01" +
         "2161" + // port
         "0000" + // nonce
@@ -82,8 +91,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
 
     it("should fail to create node if port is zero", async () => {
         await nodesFunctionality.createNode(
-            validator,
-            "0x56bc75e2d63100000",
+            0,
             "0x01" +
             "0000" + // port
             "0000" + // nonce
@@ -97,8 +105,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
 
     it("should create node", async () => {
         await nodesFunctionality.createNode(
-            validator,
-            "0x56bc75e2d63100000",
+            0,
             "0x01" +
             "2161" + // port
             "0000" + // nonce
@@ -109,21 +116,20 @@ contract("NodesFunctionality", ([owner, validator]) => {
             "6432"); // name
 
         const node = await nodesData.nodes(0);
-        node[0].should.be.equal("d2");
-        node[1].should.be.equal("0x7f000001");
+        node[1].should.be.equal("d2");
         node[2].should.be.equal("0x7f000001");
-        node[3].should.be.deep.equal(web3.utils.toBN(8545));
-        node[4].should.be.equal(
+        node[3].should.be.equal("0x7f000001");
+        node[4].should.be.deep.equal(web3.utils.toBN(8545));
+        node[5].should.be.equal(
             "0x1122334455667788990011223344556677889900112233445566778899001122" +
             "1122334455667788990011223344556677889900112233445566778899001122");
-        node[8].should.be.deep.equal(web3.utils.toBN(0));
+        node[9].should.be.deep.equal(web3.utils.toBN(0));
     });
 
     describe("when node is created", async () => {
         beforeEach(async () => {
             await nodesFunctionality.createNode(
-                validator,
-                "0x56bc75e2d63100000",
+                0,
                 "0x01" +
                 "2161" + // port
                 "0000" + // nonce
@@ -152,12 +158,12 @@ contract("NodesFunctionality", ([owner, validator]) => {
             await nodesData.numberOfActiveNodes().should.be.eventually.deep.equal(web3.utils.toBN(0));
         });
 
-        it("should fail to wethdraw deposit for non existing node", async () => {
+        it("should fail to withdraw deposit for non existing node", async () => {
             await nodesFunctionality.initWithdrawDeposit(validator, 1)
                 .should.be.eventually.rejectedWith("Node does not exist for message sender");
 
             await nodesFunctionality.initWithdrawDeposit(owner, 0)
-                .should.be.eventually.rejectedWith("Node does not exist for message sender");
+                .should.be.eventually.rejectedWith("Validator with such address doesn't exist");
         });
 
         it("should init withdrawing deposit", async () => {
@@ -168,7 +174,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
 
         it("should complete withdrawing deposit", async () => {
             await nodesFunctionality.completeWithdrawDeposit(owner, 0)
-                .should.be.eventually.rejectedWith("Node does not exist for message sender");
+                .should.be.eventually.rejectedWith("Validator with such address doesn't exist");
 
             await nodesFunctionality.completeWithdrawDeposit(validator, 1)
                 .should.be.eventually.rejectedWith("Node does not exist for message sender");
@@ -179,22 +185,21 @@ contract("NodesFunctionality", ([owner, validator]) => {
             await nodesFunctionality.initWithdrawDeposit(validator, 0);
 
             await nodesFunctionality.completeWithdrawDeposit(validator, 0)
-                .should.be.eventually.rejectedWith("eaving period is not expired");
+                .should.be.eventually.rejectedWith("Leaving period is not expired");
 
             skipTime(web3, 5);
 
             await nodesFunctionality.completeWithdrawDeposit(validator, 0);
 
             const node = await nodesData.nodes(0);
-            node[8].should.be.deep.equal(web3.utils.toBN(2));
+            node[9].should.be.deep.equal(web3.utils.toBN(2));
         });
     });
 
     describe("when two nodes are created", async () => {
         beforeEach(async () => {
             await nodesFunctionality.createNode(
-                validator,
-                "0x56bc75e2d63100000",
+                0,
                 "0x01" +
                 "2161" + // port
                 "0000" + // nonce
@@ -204,8 +209,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
                 "1122334455667788990011223344556677889900112233445566778899001122" + // public key
                 "6432"); // name
             await nodesFunctionality.createNode(
-                validator,
-                "0x56bc75e2d63100000",
+                0,
                 "0x01" +
                 "2161" + // port
                 "0000" + // nonce
@@ -242,7 +246,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
 
         it("should complete withdrawing deposit from first node", async () => {
             await nodesFunctionality.completeWithdrawDeposit(owner, 0)
-                .should.be.eventually.rejectedWith("Node does not exist for message sender");
+                .should.be.eventually.rejectedWith("Validator with such address doesn't exist");
 
             await nodesFunctionality.completeWithdrawDeposit(validator, 0)
                 .should.be.eventually.rejectedWith("Node is no Leaving");
@@ -257,12 +261,12 @@ contract("NodesFunctionality", ([owner, validator]) => {
             await nodesFunctionality.completeWithdrawDeposit(validator, 0);
 
             const node = await nodesData.nodes(0);
-            node[8].should.be.deep.equal(web3.utils.toBN(2));
+            node[9].should.be.deep.equal(web3.utils.toBN(2));
         });
 
         it("should complete withdrawing deposit from second node", async () => {
             await nodesFunctionality.completeWithdrawDeposit(owner, 1)
-                .should.be.eventually.rejectedWith("Node does not exist for message sender");
+                .should.be.eventually.rejectedWith("Validator with such address doesn't exist");
 
             await nodesFunctionality.completeWithdrawDeposit(validator, 1)
                 .should.be.eventually.rejectedWith("Node is no Leaving");
@@ -277,7 +281,7 @@ contract("NodesFunctionality", ([owner, validator]) => {
             await nodesFunctionality.completeWithdrawDeposit(validator, 1);
 
             const node = await nodesData.nodes(1);
-            node[8].should.be.deep.equal(web3.utils.toBN(2));
+            node[9].should.be.deep.equal(web3.utils.toBN(2));
         });
     });
 
