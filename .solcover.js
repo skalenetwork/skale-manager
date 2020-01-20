f01b3a8eef10fb68c9f7c8ee9befcfa8684a627a
Artem Payvin <payvinartem@gmail.com>
2020-01-20 15:19:59 +0200

    Turn on all tests

--- .solcover.js
+++ .solcover.js
@@ -1,8 +1,8 @@
 module.exports = {    
     compileCommand: '../node_modules/.bin/truffle compile --network coverage',
-    // testCommand: '../node_modules/.bin/truffle test --network coverage --gas_multiplier 10',
+    testCommand: '../node_modules/.bin/truffle test --network coverage --gas_multiplier 10',
     /// TODO: Temporary have turned off all tests and left only those who should pass
-    testCommand: '../node_modules/.bin/truffle test test/delegation/* --network coverage --gas_multiplier 10',
+    // testCommand: '../node_modules/.bin/truffle test test/delegation/* --network coverage --gas_multiplier 10',
     norpc: true,
     skipFiles: ['Migrations.sol'],
     copyPackages: ['@openzeppelin/contracts']
