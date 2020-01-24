dd5242c848e12c90f16c6f4833469562539c6472
Dmytro Stebaiev <dmytro@skalelabs.com>
2020-01-24 15:43:57 +0200

    SKALE-2015 Rename validators with monitors

--- contracts/MonitorsFunctionality.sol
+++ contracts/MonitorsFunctionality.sol
@@ -1,5 +1,5 @@
 /*
-    ValidatorsFunctionality.sol - SKALE Manager
+    MonitorsFunctionality.sol - SKALE Manager
     Copyright (C) 2018-Present SKALE Labs
     @author Artem Payvin
 
@@ -22,39 +22,28 @@ pragma solidity ^0.5.3;
 import "./GroupsFunctionality.sol";
 import "./interfaces/IConstants.sol";
 import "./interfaces/INodesData.sol";
-import "./interfaces/IValidatorsFunctionality.sol";
-
-interface IValidatorsData {
-    function addValidatedNode(bytes32 validatorIndex, bytes32 data) external;
-    function addVerdict(bytes32 validatorIndex, uint32 downtime, uint32 latency) external;
-    function removeValidatedNode(bytes32 validatorIndex, uint indexOfValidatedNode) external;
-    function removeAllValidatedNodes(bytes32 validatorIndex) external;
-    function removeAllVerdicts(bytes32 validatorIndex) external;
-    function getValidatedArray(bytes32 validatorIndex) external view returns (bytes32[] memory);
-    function getLengthOfMetrics(bytes32 validatorIndex) external view returns (uint);
-    function verdicts(bytes32 validatorIndex, uint numberOfVerdict, uint layer) external view returns (uint32);
-}
+import "./MonitorsData.sol";
 
 
-contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionality {
+contract MonitorsFunctionality is GroupsFunctionality {
 
-    event ValidatorCreated(
+    event MonitorCreated(
         uint nodeIndex,
         bytes32 groupIndex,
-        uint numberOfValidators,
+        uint numberOfMonitors,
         uint32 time,
         uint gasSpend
     );
 
-    event ValidatorUpgraded(
+    event MonitorUpgraded(
         uint nodeIndex,
         bytes32 groupIndex,
-        uint numberOfValidators,
+        uint numberOfMonitors,
         uint32 time,
         uint gasSpend
     );
 
-    event ValidatorsArray(
+    event MonitorsArray(
         uint nodeIndex,
         bytes32 groupIndex,
         uint[] nodesInGroup,
@@ -63,7 +52,7 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
     );
 
     event VerdictWasSent(
-        uint indexed fromValidatorIndex,
+        uint indexed fromMonitorIndex,
         uint indexed toNodeIndex,
         uint32 downtime,
         uint32 latency,
@@ -88,7 +77,7 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
     );
 
 
-    event ValidatorRotated(
+    event MonitorRotated(
         bytes32 groupIndex,
         uint newNode
     );
@@ -109,16 +98,16 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
     }
 
     /**
-     * addValidator - setup validators of node
+     * addMonitor - setup monitors of node
      */
-    function addValidator(uint nodeIndex) external allow(executorName) {
+    function addMonitor(uint nodeIndex) external allow(executorName) {
         address constantsAddress = contractManager.getContract("Constants");
         IConstants constantsHolder = IConstants(constantsAddress);
         bytes32 groupIndex = keccak256(abi.encodePacked(nodeIndex));
-        uint possibleNumberOfNodes = constantsHolder.NUMBER_OF_VALIDATORS();
+        uint possibleNumberOfNodes = constantsHolder.NUMBER_OF_MONITORS();
         addGroup(groupIndex, possibleNumberOfNodes, bytes32(nodeIndex));
-        uint numberOfNodesInGroup = setValidators(groupIndex, nodeIndex);
-        emit ValidatorCreated(
+        uint numberOfNodesInGroup = setMonitors(groupIndex, nodeIndex);
+        emit MonitorCreated(
             nodeIndex,
             groupIndex,
             numberOfNodesInGroup,
@@ -126,14 +115,14 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
         );
     }
 
-    function upgradeValidator(uint nodeIndex) external allow(executorName) {
+    function upgradeMonitor(uint nodeIndex) external allow(executorName) {
         address constantsAddress = contractManager.getContract("Constants");
         IConstants constantsHolder = IConstants(constantsAddress);
         bytes32 groupIndex = keccak256(abi.encodePacked(nodeIndex));
-        uint possibleNumberOfNodes = constantsHolder.NUMBER_OF_VALIDATORS();
+        uint possibleNumberOfNodes = constantsHolder.NUMBER_OF_MONITORS();
         upgradeGroup(groupIndex, possibleNumberOfNodes, bytes32(nodeIndex));
-        uint numberOfNodesInGroup = setValidators(groupIndex, nodeIndex);
-        emit ValidatorUpgraded(
+        uint numberOfNodesInGroup = setMonitors(groupIndex, nodeIndex);
+        emit MonitorUpgraded(
             nodeIndex,
             groupIndex,
             numberOfNodesInGroup,
@@ -141,35 +130,35 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
         );
     }
 
-    function deleteValidatorByRoot(uint nodeIndex) external allow(executorName) {
+    function deleteMonitorByRoot(uint nodeIndex) external allow(executorName) {
         bytes32 groupIndex = keccak256(abi.encodePacked(nodeIndex));
-        address dataAddress = contractManager.contracts(keccak256(abi.encodePacked(dataName)));
-        IValidatorsData(dataAddress).removeAllVerdicts(groupIndex);
-        IValidatorsData(dataAddress).removeAllValidatedNodes(groupIndex);
+        MonitorsData data = MonitorsData(contractManager.getContract("MonitorsData"));
+        data.removeAllVerdicts(groupIndex);
+        data.removeAllCheckedNodes(groupIndex);
         deleteGroup(groupIndex);
     }
 
     function sendVerdict(
-        uint fromValidatorIndex,
+        uint fromMonitorIndex,
         uint toNodeIndex,
         uint32 downtime,
         uint32 latency) external allow(executorName)
     {
         uint index;
         uint32 time;
-        bytes32 validatorIndex = keccak256(abi.encodePacked(fromValidatorIndex));
-        (index, time) = find(validatorIndex, toNodeIndex);
-        require(time > 0, "Validated Node does not exist in ValidatorsArray");
+        bytes32 monitorIndex = keccak256(abi.encodePacked(fromMonitorIndex));
+        (index, time) = find(monitorIndex, toNodeIndex);
+        require(time > 0, "Checked Node does not exist in MonitorsArray");
         require(time <= block.timestamp, "The time has not come to send verdict");
-        address dataAddress = contractManager.contracts(keccak256(abi.encodePacked(dataName)));
-        IValidatorsData(dataAddress).removeValidatedNode(validatorIndex, index);
+        MonitorsData data = MonitorsData(contractManager.getContract("MonitorsData"));
+        data.removeCheckedNode(monitorIndex, index);
         address constantsAddress = contractManager.getContract("Constants");
         bool receiveVerdict = time + IConstants(constantsAddress).deltaPeriod() > uint32(block.timestamp);
         if (receiveVerdict) {
-            IValidatorsData(dataAddress).addVerdict(keccak256(abi.encodePacked(toNodeIndex)), downtime, latency);
+            data.addVerdict(keccak256(abi.encodePacked(toNodeIndex)), downtime, latency);
         }
         emit VerdictWasSent(
-            fromValidatorIndex,
+            fromMonitorIndex,
             toNodeIndex,
             downtime,
             latency,
@@ -177,26 +166,26 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
     }
 
     function calculateMetrics(uint nodeIndex) external allow(executorName) returns (uint32 averageDowntime, uint32 averageLatency) {
-        address dataAddress = contractManager.contracts(keccak256(abi.encodePacked(dataName)));
-        bytes32 validatorIndex = keccak256(abi.encodePacked(nodeIndex));
-        uint lengthOfArray = IValidatorsData(dataAddress).getLengthOfMetrics(validatorIndex);
+        MonitorsData data = MonitorsData(contractManager.getContract("MonitorsData"));
+        bytes32 monitorIndex = keccak256(abi.encodePacked(nodeIndex));
+        uint lengthOfArray = data.getLengthOfMetrics(monitorIndex);
         uint32[] memory downtimeArray = new uint32[](lengthOfArray);
         uint32[] memory latencyArray = new uint32[](lengthOfArray);
         for (uint i = 0; i < lengthOfArray; i++) {
-            downtimeArray[i] = IValidatorsData(dataAddress).verdicts(validatorIndex, i, 0);
-            latencyArray[i] = IValidatorsData(dataAddress).verdicts(validatorIndex, i, 1);
+            downtimeArray[i] = data.verdicts(monitorIndex, i, 0);
+            latencyArray[i] = data.verdicts(monitorIndex, i, 1);
         }
         if (lengthOfArray > 0) {
             averageDowntime = median(downtimeArray);
             averageLatency = median(latencyArray);
-            IValidatorsData(dataAddress).removeAllVerdicts(validatorIndex);
+            data.removeAllVerdicts(monitorIndex);
         }
     }
 
     function rotateNode(bytes32 schainId) external allow("SkaleManager") {
         uint newNodeIndexEvent;
         newNodeIndexEvent = selectNodeToGroup(schainId);
-        emit ValidatorRotated(schainId, newNodeIndexEvent);
+        emit MonitorRotated(schainId, newNodeIndexEvent);
     }
 
     function selectNodeToGroup(bytes32 groupIndex) internal returns (uint) {
@@ -218,7 +207,7 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
             hash = uint(keccak256(abi.encodePacked(hash, indexOfNode)));
             iterations++;
         }
-        require(iterations < 200, "Old Validator is not replaced? Try it later");
+        require(iterations < 200, "Old Monitor is not replaced? Try it later");
     }
 
     function generateGroup(bytes32 groupIndex) internal allow(executorName) returns (uint[] memory) {
@@ -282,16 +271,16 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
         return INodesData(nodesDataAddress).isNodeActive(indexOfNode) && !IGroupsData(dataAddress).isExceptionNode(groupIndex, indexOfNode);
     }
 
-    function setValidators(bytes32 groupIndex, uint nodeIndex) internal returns (uint) {
-        address dataAddress = contractManager.contracts(keccak256(abi.encodePacked(dataName)));
-        IGroupsData(dataAddress).setException(groupIndex, nodeIndex);
+    function setMonitors(bytes32 groupIndex, uint nodeIndex) internal returns (uint) {
+        MonitorsData data = MonitorsData(contractManager.getContract("MonitorsData"));
+        data.setException(groupIndex, nodeIndex);
         uint[] memory indexOfNodesInGroup = generateGroup(groupIndex);
         bytes32 bytesParametersOfNodeIndex = getDataToBytes(nodeIndex);
         for (uint i = 0; i < indexOfNodesInGroup.length; i++) {
             bytes32 index = keccak256(abi.encodePacked(indexOfNodesInGroup[i]));
-            IValidatorsData(dataAddress).addValidatedNode(index, bytesParametersOfNodeIndex);
+            data.addCheckedNode(index, bytesParametersOfNodeIndex);
         }
-        emit ValidatorsArray(
+        emit MonitorsArray(
             nodeIndex,
             groupIndex,
             indexOfNodesInGroup,
@@ -300,13 +289,13 @@ contract ValidatorsFunctionality is GroupsFunctionality, IValidatorsFunctionalit
         return indexOfNodesInGroup.length;
     }
 
-    function find(bytes32 validatorIndex, uint nodeIndex) internal view returns (uint index, uint32 time) {
-        address dataAddress = contractManager.contracts(keccak256(abi.encodePacked(dataName)));
-        bytes32[] memory validatedNodes = IValidatorsData(dataAddress).getValidatedArray(validatorIndex);
+    function find(bytes32 monitorIndex, uint nodeIndex) internal view returns (uint index, uint32 time) {
+        MonitorsData data = MonitorsData(contractManager.getContract("MonitorsData"));
+        bytes32[] memory checkedNodes = data.getCheckedArray(monitorIndex);
         uint possibleIndex;
         uint32 possibleTime;
-        for (uint i = 0; i < validatedNodes.length; i++) {
-            (possibleIndex, possibleTime) = getDataFromBytes(validatedNodes[i]);
+        for (uint i = 0; i < checkedNodes.length; i++) {
+            (possibleIndex, possibleTime) = getDataFromBytes(checkedNodes[i]);
             if (possibleIndex == nodeIndex && (time == 0 || possibleTime < time)) {
                 index = i;
                 time = possibleTime;
