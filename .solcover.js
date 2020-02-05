b155428b05348c33864699ad326efbb3cabf67a7
Vadim Yavorsky <yavrsky@gmail.com>
2020-02-05 21:30:28 +0200

    SKALE-2060 added private keys to virtual environment

--- .solcover.js
+++ .solcover.js
@@ -1,34 +1,28 @@
+require('dotenv').config();
+
 module.exports = {    
-    compileCommand: '../node_modules/.bin/truffle compile --network coverage',
-    testCommand: '../node_modules/.bin/truffle test --network coverage --gas_multiplier 10',
-    norpc: true,
-    skipFiles: ['Migrations.sol'],
-    mocha: {
-        enableTimeouts: false
-    },
+    skipFiles: ['Migrations.sol', 'thirdparty/', 'interfaces/'],
     providerOptions: {
-        "port": 8555,
-        "gasLimit": "0xfffffffffff",
         "accounts": [
             {
-                "balance": "0xd3c21bcecceda1000000",
-                "secretKey": "0xa15c19da241e5b1db20d8dd8ca4b5eeaee01c709b49ec57aa78c2133d3c1b3c9"
+                "secretKey": process.env.PRIVATE_KEY_1,
+                "balance": "0xd3c21bcecceda1000000"
             },
             {
-                "balance": "0xd3c21bcecceda1000000",
-                "secretKey": "0xe7af72d241d4dd77bc080ce9234d742f6b22e35b3a660e8c197517b909f63ca8"
+                "secretKey": process.env.PRIVATE_KEY_2,
+                "balance": "0xd3c21bcecceda1000000"
             },
             {
-                "balance": "0xd3c21bcecceda1000000",
-                "secretKey": "0xd87e2b83ec1cc04dbb82e144a966e3a345225af631c3c60955724b0d91f97279"
+                "secretKey": process.env.PRIVATE_KEY_3,
+                "balance": "0xd3c21bcecceda1000000"
             },
             {
-                "balance": "0xd3c21bcecceda1000000",
-                "secretKey": "0x35be08dece9f9db0e595e5144013558ce602af32cb0d73bfd901455213577bd6"
+                "secretKey": process.env.PRIVATE_KEY_4,
+                "balance": "0xd3c21bcecceda1000000"
             },
             {
-                "balance": "0xd3c21bcecceda0000000",
-                "secretKey": "0xe8829ca5780908bb7e842eb69077410c9e89ed323c8038a634d39c0b301e32f8"
+                "secretKey": process.env.PRIVATE_KEY_5,
+                "balance": "0xd3c21bcecceda0000000"
             }
         ]
     }
