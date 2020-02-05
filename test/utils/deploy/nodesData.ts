fcf7bf650a3f37ac9921453c26bf98bd1692289f
Dmytro Stebaiev <dmytro@skalelabs.com>
2020-02-05 15:12:22 +0200

    SKALE-2078 Refactor deployment scripts for tests

--- test/utils/deploy/nodesData.ts
+++ test/utils/deploy/nodesData.ts
@@ -1,25 +1,16 @@
-import { ContractManagerInstance, NodesDataContract } from "../../../types/truffle-contracts";
+import { ContractManagerInstance, NodesDataContract, NodesDataInstance } from "../../../types/truffle-contracts";
 import { deployConstantsHolder } from "./constantsHolder";
+import { deployFunctionFactory } from "./factory";
 
 const NodesData: NodesDataContract = artifacts.require("./NodesData");
-const name = "NodesData";
 
-async function deploy(contractManager: ContractManagerInstance) {
-    const instance = await NodesData.new(5, contractManager.address);
-    await contractManager.setContractsAddress(name, instance.address);
-    return instance;
-}
+const deployNodesData: (contractManager: ContractManagerInstance) => Promise<NodesDataInstance>
+    = deployFunctionFactory("NodesData",
+                            async (contractManager: ContractManagerInstance) => {
+                                await deployConstantsHolder(contractManager);
+                            },
+                            async (contractManager: ContractManagerInstance) => {
+                                return await NodesData.new(5, contractManager.address);
+                            });
 
-async function deployDependencies(contractManager: ContractManagerInstance) {
-    await deployConstantsHolder(contractManager);
-}
-
-export async function deployNodesData(contractManager: ContractManagerInstance) {
-    try {
-        return NodesData.at(await contractManager.getContract(name));
-    } catch (e) {
-        const instance = await deploy(contractManager);
-        await deployDependencies(contractManager);
-        return instance;
-    }
-}
+export { deployNodesData };
