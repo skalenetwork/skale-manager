c932a84a6041f73c086b24e6718b2bff2e14258c
Vadim Yavorsky <yavrsky@gmail.com>
2020-02-19 19:41:59 +0200

    SKALE-2144 separate tests into 3 folders

---
+++ scripts/move.sh
@@ -0,0 +1,7 @@
+cd test
+mkdir 1
+cp SkaleManager.ts 1/ &&
+mkdir 2
+cp SchainsFunctionality.ts 2/ && cp SkaleDKG.ts 2/ && cp MonitorsFunctionality.spec.ts 2/ && cp SchainsData.ts 2/ && cp ECDH.ts 2/ &&
+mkdir 3
+cp -r delegation/ 3/ && cp SkaleToken.ts 3/ && cp Pricing.ts 3/ && cp NodesFunctionality.ts 3/ && cp SkaleVerifier.ts 3/ && cp MonitorsData.ts 3/ && cp NodesData.ts 3/ && cp ConstantsHolder.spec.ts 3/ && cp Decryption.ts 3/ && cp ManagerData.spec.ts 3/ && cp ContractManager.ts 3/
