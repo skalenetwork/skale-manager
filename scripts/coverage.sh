ff496b0087f785896844ac2888cf4e9cfc935ea4
Vadim Yavorsky <yavrsky@gmail.com>
2020-02-24 18:47:46 +0200

    splitted tests in 3 bunches

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,11 +1,9 @@
-# for file in test/$TESTFOLDERS/*
-# do
-# file=${file:7}
-# if [ ! -f "test/$file" ]; then
-#     file="delegation/$file"
-# fi
-# npx buidler coverage --testfiles test/$file --solcoverjs .solcover.js
-# bash <(curl -s https://codecov.io/bash) -cF solidity
-# done
-str='101.1'
-echo ${str: -2}
+for file in test/$TESTFOLDERS/*
+do
+file=${file:7}
+if [ ! -f "test/$file" ]; then
+    file="delegation/$file"
+fi
+npx buidler coverage --testfiles test/$file --solcoverjs .solcover.js
+bash <(curl -s https://codecov.io/bash) -cF solidity
+done
