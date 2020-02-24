16a93ffbda1d1b3899957b1b6d8184fb9f0c0ff4
Vadim Yavorsky <yavrsky@gmail.com>
2020-02-24 21:36:32 +0200

    turned on linter on travis

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,5 +5,5 @@ if [ ! -f "test/$file" ]; then
     file="delegation/$file"
 fi
 npx buidler coverage --testfiles test/$file --solcoverjs .solcover.js
-bash <(curl -s https://codecov.io/bash) -cF solidity
+bash <(curl -s https://codecov.io/bash)
 done
--- scripts/move.sh
+++ scripts/move.sh
@@ -7,6 +7,9 @@ cp SkaleDKG.ts 2/
 cp MonitorsFunctionality.spec.ts 2/ 
 cp SchainsData.ts 2/ 
 cp ECDH.ts 2/
+cp Decryption.ts 2/
+cp ManagerData.spec.ts 2/
+cp ContractManager.ts 2/
 mkdir 3
 cp -r delegation/* 3/
 cp SkaleToken.ts 3/
@@ -16,6 +19,3 @@ cp SkaleVerifier.ts 3/
 cp MonitorsData.ts 3/
 cp NodesData.ts 3/
 cp ConstantsHolder.spec.ts 3/
-cp Decryption.ts 3/
-cp ManagerData.spec.ts 3/
-cp ContractManager.ts 3/
