baf7f351b5d4a0445e2a9f92af55c64f5cf0a31f
Vadim Yavorsky <yavrsky@gmail.com>
2020-03-10 15:44:20 +0200

    fixed emitting errors on travis

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,6 +4,6 @@ file=${file:7}
 if [ ! -f "test/$file" ]; then
     file="delegation/$file"
 fi
-npx buidler coverage --testfiles test/$file --solcoverjs .solcover.js
+npx buidler coverage --testfiles test/$file --solcoverjs .solcover.js || travis_terminate 4
 bash <(curl -s https://codecov.io/bash)
 done
