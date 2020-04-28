fcf88525c765b1c443dd897176de39fe86ffece2
Dmytro Stebaiev <dimastebaev@gmail.com>
2020-04-28 17:18:44 +0300

    SKALE-2477 Fix SchainsData

--- contracts/SchainsData.sol
+++ contracts/SchainsData.sol
@@ -141,7 +141,7 @@ contract SchainsData is GroupsData {
                 holesForNodes[nodeIndex][0] = min;
                 holesForNodes[nodeIndex][index] = holesForNodes[nodeIndex][holesForNodes[nodeIndex].length - 1];
                 delete holesForNodes[nodeIndex][holesForNodes[nodeIndex].length - 1];
-                holesForNodes[nodeIndex].length--;
+                holesForNodes[nodeIndex].pop();
             }
         }
     }
@@ -186,7 +186,7 @@ contract SchainsData is GroupsData {
             schainIndexes[from][index] = lastSchainId;
         }
         delete schainIndexes[from][length - 1];
-        schainIndexes[from].length--;
+        schainIndexes[from].pop();
 
         // TODO:
         // optimize
@@ -197,7 +197,7 @@ contract SchainsData is GroupsData {
             }
         }
         delete schainsAtSystem[schainsAtSystem.length - 1];
-        schainsAtSystem.length--;
+        schainsAtSystem.pop();
 
         delete schains[schainId];
         numberOfSchains--;
@@ -213,7 +213,7 @@ contract SchainsData is GroupsData {
         uint length = schainsForNodes[nodeIndex].length;
         if (schainIndex == length - 1) {
             delete schainsForNodes[nodeIndex][length - 1];
-            schainsForNodes[nodeIndex].length--;
+            schainsForNodes[nodeIndex].pop();
         } else {
             schainsForNodes[nodeIndex][schainIndex] = bytes32(0);
             if (holesForNodes[nodeIndex].length > 0 && holesForNodes[nodeIndex][0] > schainIndex) {
