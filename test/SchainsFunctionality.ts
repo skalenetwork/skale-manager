c0d2a35bac4cf4825fc30c5ced4d37f8ee67f1e2
Artem Payvin <payvinartem@gmail.com>
2020-05-16 01:30:46 +0300

    Fixed errors

--- test/SchainsFunctionality.ts
+++ test/SchainsFunctionality.ts
@@ -829,12 +829,12 @@ contract("SchainsFunctionality", ([owner, holder, validator]) => {
             console.log(res1);
             res = await skaleDKG.isBroadcastPossible(web3.utils.soliditySha3("d3"), nodeRot, {from: validator});
             assert.equal(res, true);
-            const verificationVector = 
+            const verificationVector =
                 "0x02c2b888a23187f22195eadadbc05847a00dc59c913d465dbc4dfac9cfab437d2695832627b9081e77da7a3fc4d574363bf05" +
                 "1700055822f3d394dc3d9ff741724727c45f9322be756fbec6514525cbbfa27ef1951d3fed10f483c23f921879d03a7a3e6f3b5" +
                 "39dad43c0eca46e3f889b2b2300815ffc4633e26e64406625a99";
 
-            const encryptedSecretKeyContribution = 
+            const encryptedSecretKeyContribution =
                 "0x937c9c846a6fa7fd1984fe82e739ae37fcaa555c1dc0e8597c9f81b6a12f232f04fdf8101e91bd658fa1cea6fdd75adb85429" +
                 "51ce3d251cdaa78f43493dad730b59d32d2e872b36aa70cdce544b550ebe96994de860b6f6ebb7d0b4d4e6724b4bf7232f27fdf" +
                 "e521f3c7997dbb1c15452b7f196bd119d915ce76af3d1a008e181004086ff076abe442563ae9b8938d483ae581f4de2ee54298b" +
