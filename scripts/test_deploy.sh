eaab70845748ef0fbcedc4bf7f6e2db2a3a56f71
Dmytro Stebaiev <dmytro@skalelabs.com>
2020-07-07 17:49:36 +0300

    SKALE-2853 Fix deploy

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -2,6 +2,7 @@
 
 npx ganache-cli --gasLimit 8000000 --quiet &
 GANACHE_PID=$!
+npx oz push --network test --force || exit $?
 npx truffle migrate --network test || exit $?
 sleep 5
 kill $GANACHE_PID
