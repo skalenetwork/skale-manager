3d64c575fb4eb84f927107dc10a16c1ee90b6579
Dmytro Stebaiev <dmytro@skalelabs.com>
2020-07-07 15:24:23 +0300

    SKALE-2853 Add script to check upgrade

---
+++ scripts/test_upgrade.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+VERSION=$(cat VERSION)
+DEPLOYED_VERSION=$(cat $TRAVIS_BUILD_DIR/DEPLOYED)
+DEPLOYED_DIR=$TRAVIS_BUILD_DIR/deployed-skale-manager/
+
+git clone --branch $DEPLOYED_VERSION https://github.com/$TRAVIS_REPO_SLUG.git $DEPLOYED_DIR
+
+npx ganache-cli --gasLimit 8000000 --quiet &
+GANACHE_PID=$!
+
+cd $DEPLOYED_DIR
+yarn install
+npx truffle migrate --network test || exit $?
+cp .openzeppelin/dev-*.json $TRAVIS_BUILD_DIR/.openzeppelin
+cd $TRAVIS_BUILD_DIR
+
+npx oz upgrade --network test --all
+
+kill $GANACHE_PID
