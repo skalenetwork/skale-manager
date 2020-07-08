0c8aedca4b86aed196d4c5175f749531cc166528
Dmytro Stebaiev <dmytro@skalelabs.com>
2020-07-08 10:12:26 +0300

    SKALE-2853 Increase heap

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -3,6 +3,6 @@
 npx ganache-cli --gasLimit 8000000 --quiet &
 GANACHE_PID=$!
 npx oz push --network test --force || exit $?
-npx truffle migrate --network test || exit $?
+NODE_OPTIONS="--max-old-space-size=4096" npx truffle migrate --network test || exit $?
 sleep 5
 kill $GANACHE_PID
