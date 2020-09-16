00f6bcb7eb281aa93d21e16170de6f3132ec4356
Vadim Yavorsky <yavrsky@gmail.com>
2020-09-16 14:26:14 +0300

    SKALE-3220 Add private methods for setting Node status

--- contracts/Nodes.sol
+++ contracts/Nodes.sol
@@ -344,8 +344,7 @@ contract Nodes is Permissions {
             permitted = validatorService.getValidatorId(msg.sender) == validatorId;
         }
         require(permitted, "Sender is not permitted to call this function");
-        nodes[nodeIndex].status = NodeStatus.In_Maintenance;
-        numberOfActiveNodes--;
+        _setNodeInMaintenance(nodeIndex);
     }
 
     function removeNodeFromInMaintenance(uint nodeIndex) external {
@@ -357,8 +356,7 @@ contract Nodes is Permissions {
             permitted = validatorService.getValidatorId(msg.sender) == validatorId;
         }
         require(permitted, "Sender is not permitted to call this function");
-        nodes[nodeIndex].status = NodeStatus.Active;
-        numberOfActiveNodes++;
+        _setNodeActive(nodeIndex);
     }
 
     function getNodesWithFreeSpace(uint8 freeSpace) external view returns (uint[] memory) {
@@ -663,6 +661,26 @@ contract Nodes is Permissions {
         spaceToNodes[newSpace].push(nodeIndex);
         spaceOfNodes[nodeIndex].freeSpace = newSpace;
         spaceOfNodes[nodeIndex].indexInSpaceMap = spaceToNodes[newSpace].length.sub(1);
+    }
+
+    /**
+     * @dev _setNodeActive - set Node Active
+     * function could be run only by Nodes
+     * @param nodeIndex - index of Node
+     */
+    function _setNodeActive(uint nodeIndex) private {
+        nodes[nodeIndex].status = NodeStatus.Active;
+        numberOfActiveNodes = numberOfActiveNodes.add(1);
+    }
+
+    /**
+     * @dev _setNodeInMaintenance - set Node in Maintenance
+     * function could be run only by Nodes
+     * @param nodeIndex - index of Node
+     */
+    function _setNodeInMaintenance(uint nodeIndex) private {
+        nodes[nodeIndex].status = NodeStatus.In_Maintenance;
+        numberOfActiveNodes = numberOfActiveNodes.sub(1);
     }
 
     /**
