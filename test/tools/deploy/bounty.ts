c62adcc29ef1524088aaeea6c194c7dd8560cc67
Dmytro Stebaiev <dimastebaev@gmail.com>
2020-10-24 17:32:53 +0300

    SKALE-3273 Fix BountyV2 deployment

--- test/tools/deploy/bounty.ts
+++ test/tools/deploy/bounty.ts
@@ -11,15 +11,6 @@ const deployBounty: (contractManager: ContractManagerInstance) => Promise<Bounty
                                 await deployConstantsHolder(contractManager);
                                 await deployNodes(contractManager);
                                 await deployTimeHelpers(contractManager);
-                            },
-                            async(contractManager: ContractManagerInstance) => {
-                                const BountyV2: BountyV2Contract = artifacts.require("./BountyV2");
-                                const instance = await BountyV2.new();
-                                // some contracts have to be deployed before BountyV2 initialization
-                                await deployTimeHelpers(contractManager);
-                                await deployDelegationController(contractManager);
-                                await instance.initialize(contractManager.address, 0);
-                                return instance;
                             });
 
 export { deployBounty };
