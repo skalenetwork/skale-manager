33257781a2b49282f1c882224552f70b0a29861d
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-02-10 18:24:40 +0200

    SKALE-3844 Increase memory limit for test coverate calculation

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-npx hardhat coverage --solcoverjs .solcover.js || exit $?
+NODE_OPTIONS="--max_old_space_size=4096" npx hardhat coverage --solcoverjs .solcover.js || exit $?
 bash <(curl -s https://codecov.io/bash)
