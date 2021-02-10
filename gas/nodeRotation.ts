fc893d86602b2c3937463de863d7bdfb6e2a4d84
Artem Payvin <payvinartem@gmail.com>
2021-02-10 20:56:04 +0200

    SKALE-3513 fix node rotation test

--- gas/nodeRotation.ts
+++ gas/nodeRotation.ts
@@ -2,32 +2,41 @@ import { deployContractManager } from "../test/tools/deploy/contractManager";
 import { deployValidatorService } from "../test/tools/deploy/delegation/validatorService";
 import { deploySkaleManager } from "../test/tools/deploy/skaleManager";
 import {
-    ContractManagerInstance,
-    SchainsInstance,
-    SchainsInternalInstance,
-    SkaleDKGTesterInstance,
-    SkaleManagerInstance,
-    ValidatorServiceInstance
-} from "../types/truffle-contracts";
+    ContractManager,
+    Schains,
+    SchainsInternal,
+    SkaleDKGTester,
+    SkaleManager,
+    ValidatorService
+} from "../typechain";
 import { privateKeys } from "../test/tools/private-keys";
 import * as elliptic from "elliptic";
 import { deploySchains } from "../test/tools/deploy/schains";
 import { deploySchainsInternal } from "../test/tools/deploy/schainsInternal";
 import { deploySkaleDKGTester } from "../test/tools/deploy/test/skaleDKGTester";
 import { skipTime, currentTime } from "../test/tools/time";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { ethers, web3 } from "hardhat";
 import fs from 'fs';
 
 const ec = new elliptic.ec("secp256k1");
 
-contract("createSchains", ([owner, validator, node]) => {
-    let contractManager: ContractManagerInstance;
-    let validatorService: ValidatorServiceInstance;
-    let skaleManager: SkaleManagerInstance;
-    let schains: SchainsInstance;
-    let schainsInternal: SchainsInternalInstance;
-    let skaleDKG: SkaleDKGTesterInstance;
+describe("createSchains", () => {
+    let owner: SignerWithAddress;
+    let validator: SignerWithAddress;
+    let node: SignerWithAddress;
+
+    let contractManager: ContractManager;
+    let validatorService: ValidatorService;
+    let skaleManager: SkaleManager;
+    let schains: Schains;
+    let schainsInternal: SchainsInternal;
+    let skaleDKG: SkaleDKGTester;
 
     beforeEach(async () => {
+        [owner, validator, node] = await ethers.getSigners();
+        contractManager = await deployContractManager();
+
         contractManager = await deployContractManager();
         skaleDKG = await deploySkaleDKGTester(contractManager);
 
@@ -41,26 +50,26 @@ contract("createSchains", ([owner, validator, node]) => {
     it("64 node rotations on 17 nodes", async () => {
         const validatorId = 1;
 
-        await validatorService.registerValidator("Validator", "", 0, 0, {from: validator});
+        await validatorService.connect(validator).registerValidator("Validator", "", 0, 0);
         await validatorService.disableWhitelist();
         let signature = await web3.eth.sign(web3.utils.soliditySha3(validatorId.toString()), node);
         signature = (signature.slice(130) === "00" ? signature.slice(0, 130) + "1b" :
                 (signature.slice(130) === "01" ? signature.slice(0, 130) + "1c" : signature));
-        await validatorService.linkNodeAddress(node, signature, {from: validator});
+        await validatorService.connect(validator).linkNodeAddress(node, signature);
         await schains.grantRole(await schains.SCHAIN_CREATOR_ROLE(), owner);
 
         const nodesAmount = 16;
         const publicKey = ec.keyFromPrivate(String(privateKeys[2]).slice(2)).getPublic();
         for (let nodeId = 0; nodeId < nodesAmount; ++nodeId) {
-            await skaleManager.createNode(
+            await skaleManager.connect(node).createNode(
                 1, // port
                 0, // nonce
                 "0x7f" + ("000000" + nodeId.toString(16)).slice(-6), // ip
                 "0x7f" + ("000000" + nodeId.toString(16)).slice(-6), // public ip
                 ["0x" + publicKey.x.toString('hex'), "0x" + publicKey.y.toString('hex')], // public key
                 "d2-" + nodeId, // name)
-                "somedomain.name",
-            {from: node});
+                "somedomain.name"
+            );
         }
 
         const numberOfSchains = 64;
@@ -72,15 +81,15 @@ contract("createSchains", ([owner, validator, node]) => {
             console.log("create", schainNumber + 1, "schain on", nodesAmount, "nodes:\t", result.receipt.gasUsed, "gu");
         }
 
-        await skaleManager.createNode(
+        await skaleManager.connect(node).createNode(
             1, // port
             0, // nonce
             "0x7f" + ("000000" + Number(16).toString(16)).slice(-6), // ip
             "0x7f" + ("000000" + Number(16).toString(16)).slice(-6), // public ip
             ["0x" + publicKey.x.toString('hex'), "0x" + publicKey.y.toString('hex')], // public key
             "d2-16", // name)
-            "somedomain.name",
-        {from: node});
+            "somedomain.name"
+        );
 
         const gasLimit = 12e6;
         const rotIndex = Math.floor(Math.random() * nodesAmount);
@@ -89,7 +98,7 @@ contract("createSchains", ([owner, validator, node]) => {
         console.log("Will process", schainIds.length, "rotations");
         const gas = [];
         for (let i = 0; i < schainIds.length; i++) {
-            const result = await skaleManager.nodeExit(rotIndex, {from: node});
+            const result = await skaleManager.connect(node).nodeExit(rotIndex);
             console.log("" + (i + 1) + "", "Rotation on", nodesAmount, "nodes:\t", result.receipt.gasUsed, "gu");
             gas.push(result.receipt.gasUsed);
             if (result.receipt.gasUsed > gasLimit) {
@@ -104,26 +113,26 @@ contract("createSchains", ([owner, validator, node]) => {
     it("max node rotation on 17 nodes", async () => {
         const validatorId = 1;
 
-        await validatorService.registerValidator("Validator", "", 0, 0, {from: validator});
+        await validatorService.connect(validator).registerValidator("Validator", "", 0, 0);
         await validatorService.disableWhitelist();
         let signature = await web3.eth.sign(web3.utils.soliditySha3(validatorId.toString()), node);
         signature = (signature.slice(130) === "00" ? signature.slice(0, 130) + "1b" :
                 (signature.slice(130) === "01" ? signature.slice(0, 130) + "1c" : signature));
-        await validatorService.linkNodeAddress(node, signature, {from: validator});
+        await validatorService.connect(validator).linkNodeAddress(node, signature);
         await schains.grantRole(await schains.SCHAIN_CREATOR_ROLE(), owner);
 
         const nodesAmount = 16;
         const publicKey = ec.keyFromPrivate(String(privateKeys[2]).slice(2)).getPublic();
         for (let nodeId = 0; nodeId < nodesAmount; ++nodeId) {
-            await skaleManager.createNode(
+            await skaleManager.connect(node).createNode(
                 1, // port
                 0, // nonce
                 "0x7f" + ("000000" + nodeId.toString(16)).slice(-6), // ip
                 "0x7f" + ("000000" + nodeId.toString(16)).slice(-6), // public ip
                 ["0x" + publicKey.x.toString('hex'), "0x" + publicKey.y.toString('hex')], // public key
                 "d2-" + nodeId, // name)
-                "somedomain.name",
-            {from: node});
+                "somedomain.name"
+            );
         }
 
         const numberOfSchains = 128;
@@ -138,15 +147,15 @@ contract("createSchains", ([owner, validator, node]) => {
         // await schains.addSchainByFoundation(0, 1, 0, "schain-128", owner)
         //     .should.be.eventually.rejectedWith("Not enough nodes to create Schain");
 
-        await skaleManager.createNode(
+        await skaleManager.connect(node).createNode(
             1, // port
             0, // nonce
             "0x7f" + ("000000" + Number(16).toString(16)).slice(-6), // ip
             "0x7f" + ("000000" + Number(16).toString(16)).slice(-6), // public ip
             ["0x" + publicKey.x.toString('hex'), "0x" + publicKey.y.toString('hex')], // public key
             "d2-16", // name)
-            "somedomain.name",
-        {from: node});
+            "somedomain.name"
+        );
 
         const gasLimit = 12e6;
         const rotIndex = Math.floor(Math.random() * nodesAmount);
@@ -155,7 +164,7 @@ contract("createSchains", ([owner, validator, node]) => {
         console.log("Will process", schainIds.length, "rotations");
         const gas = [];
         for (let i = 0; i < schainIds.length; i++) {
-            const result = await skaleManager.nodeExit(rotIndex, {from: node});
+            const result = await skaleManager.connect(node).nodeExit(rotIndex);
             console.log("" + (i + 1) + "", "Rotation on", nodesAmount, "nodes:\t", result.receipt.gasUsed, "gu");
             gas.push(result.receipt.gasUsed);
             if (result.receipt.gasUsed > gasLimit) {
@@ -170,12 +179,12 @@ contract("createSchains", ([owner, validator, node]) => {
     it("random rotation on dynamically creating schains", async () => {
         const validatorId = 1;
 
-        await validatorService.registerValidator("Validator", "", 0, 0, {from: validator});
+        await validatorService.connect(validator).registerValidator("Validator", "", 0, 0);
         await validatorService.disableWhitelist();
         let signature = await web3.eth.sign(web3.utils.soliditySha3(validatorId.toString()), node);
         signature = (signature.slice(130) === "00" ? signature.slice(0, 130) + "1b" :
                 (signature.slice(130) === "01" ? signature.slice(0, 130) + "1c" : signature));
-        await validatorService.linkNodeAddress(node, signature, {from: validator});
+        await validatorService.connect(validator).linkNodeAddress(node, signature);
         await schains.grantRole(await schains.SCHAIN_CREATOR_ROLE(), owner);
 
         const maxNodesAmount = 1000;
@@ -185,15 +194,15 @@ contract("createSchains", ([owner, validator, node]) => {
         const publicKey = ec.keyFromPrivate(String(privateKeys[2]).slice(2)).getPublic();
         const exitedNode = new Set();
         for (let nodeId = 0; nodeId < maxNodesAmount; ++nodeId) {
-            await skaleManager.createNode(
+            await skaleManager.connect(node).createNode(
                 1, // port
                 0, // nonce
                 "0x7f" + ("000000" + nodeId.toString(16)).slice(-6), // ip
                 "0x7f" + ("000000" + nodeId.toString(16)).slice(-6), // public ip
                 ["0x" + publicKey.x.toString('hex'), "0x" + publicKey.y.toString('hex')], // public key
                 "d2-" + nodeId, // name)
-                "somedomain.name",
-            {from: node});
+                "somedomain.name"
+            );
 
             const nodesAmount = nodeId + 1;
             if (nodesAmount >= 16) {
@@ -217,7 +226,7 @@ contract("createSchains", ([owner, validator, node]) => {
                 console.log("Will process", schainIds.length, "rotations");
                 const gas = [];
                 for (let i = 0; i < schainIds.length; i++) {
-                    const result = await skaleManager.nodeExit(rotIndex, {from: node});
+                    const result = await skaleManager.connect(node).nodeExit(rotIndex);
                     console.log("" + (i + 1) + "", "Rotation on", nodesAmount, "nodes:\t", result.receipt.gasUsed, "gu");
                     gas.push(result.receipt.gasUsed);
                     if (result.receipt.gasUsed > gasLimit) {
