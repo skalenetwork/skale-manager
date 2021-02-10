48d47370cf48f1ce8c23faa5392c308a8b121959
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-02-10 12:39:11 +0200

    SKALE-3844 Change upgrade check script

--- scripts/test_upgrade.sh
+++ scripts/test_upgrade.sh
@@ -9,15 +9,21 @@ npx ganache-cli --gasLimit 8000000 --quiet &
 GANACHE_PID=$!
 
 cd $DEPLOYED_DIR
-yarn install
+yarn install || exit $?
 npx oz push --network test --force || exit $?
 NODE_OPTIONS="--max-old-space-size=4096" PRODUCTION=true npx truffle migrate --network test || exit $?
 rm $GITHUB_WORKSPACE/.openzeppelin/dev-*.json
 cp .openzeppelin/dev-*.json $GITHUB_WORKSPACE/.openzeppelin || exit $?
+cp .openzeppelin/project.json $GITHUB_WORKSPACE/.openzeppelin || exit $?
+cp data/test.json $GITHUB_WORKSPACE/data || exit $?
 cd $GITHUB_WORKSPACE
 rm -r --interactive=never $DEPLOYED_DIR
 
-npx oz push --network test || exit $?
-npx oz upgrade --network test --all || exit $?
+NETWORK_ID=$(ls -a .openzeppelin | grep dev | cut -d '-' -f 2 | cut -d '.' -f 1)
+cp .openzeppelin/dev-$NETWORK_ID.json .openzeppelin/mainnet.json || exit $?
+
+npx migrate-oz-cli-project || exit $?
+mv .openzeppelin/mainnet.json .openzeppelin/unknown-$NETWORK_ID.json || exit $?
+ABI=data/test.json npx hardhat run migrations/upgrade.ts --network localhost || exit $?
 
 kill $GANACHE_PID
