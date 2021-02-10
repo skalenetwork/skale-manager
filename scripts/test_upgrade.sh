2a1fc91fbec11c70c9a8df97f97ffd3c68d5c9a3
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-02-10 13:30:00 +0200

    SKALE-3844 Add debug output

--- scripts/test_upgrade.sh
+++ scripts/test_upgrade.sh
@@ -20,10 +20,18 @@ cd $GITHUB_WORKSPACE
 rm -r --interactive=never $DEPLOYED_DIR
 
 NETWORK_ID=$(ls -a .openzeppelin | grep dev | cut -d '-' -f 2 | cut -d '.' -f 1)
+
+ls -a .openzeppelin
+echo $NETWORK_ID
+tail .openzeppelin/dev-$NETWORK_ID.json
+
 mv .openzeppelin/dev-$NETWORK_ID.json .openzeppelin/mainnet.json || exit $?
 
 npx migrate-oz-cli-project || exit $?
 mv .openzeppelin/mainnet.json .openzeppelin/unknown-$NETWORK_ID.json || exit $?
+
+tail .openzeppelin/unknown-$NETWORK_ID.json
+
 ABI=data/test.json npx hardhat run migrations/upgrade.ts --network localhost || exit $?
 
 kill $GANACHE_PID
