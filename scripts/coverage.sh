128c0bbaafc62ba89b7a604986ca94df45323227
Artem Payvin <payvinartem@gmail.com>
2021-02-13 12:30:07 +0200

    SKALE-3513 decrease ram for node

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-NODE_OPTIONS="--max_old_space_size=4096" npx hardhat coverage --solcoverjs .solcover.js || exit $?
+NODE_OPTIONS="--max_old_space_size=2048" npx hardhat coverage --solcoverjs .solcover.js || exit $?
 bash <(curl -s https://codecov.io/bash)
