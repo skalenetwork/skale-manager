387e908807b342ac92c720353e72f7bc89f766c0
Artem Payvin <payvinartem@gmail.com>
2021-02-13 15:28:16 +0200

    SKALE-3513 increase RAM to 3,5 GB

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-NODE_OPTIONS="--max_old_space_size=3072" npx hardhat coverage --solcoverjs .solcover.js || exit $?
+NODE_OPTIONS="--max_old_space_size=3584" npx hardhat coverage --solcoverjs .solcover.js || exit $?
 bash <(curl -s https://codecov.io/bash)
