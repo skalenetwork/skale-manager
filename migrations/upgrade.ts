820e70acf61e226e16f16e58a1001554c1c80805
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-03-02 16:37:56 +0200

    SKALE-3318 Add script to upgrade from skale-manager v1.8.0-develop.8

--- migrations/upgrade.ts
+++ migrations/upgrade.ts
@@ -9,13 +9,19 @@ import { getManifestAdmin } from "@openzeppelin/hardhat-upgrades/dist/admin";
 import { SafeMock } from "../typechain/SafeMock";
 import { encodeTransaction } from "./tools/multiSend";
 import { createMultiSendTransaction, getSafeRelayUrl, getSafeTransactionUrl, sendSafeTransaction } from "./tools/gnosis-safe";
-import axios from "axios";
 import chalk from "chalk";
 import { verify, verifyProxy } from "./tools/verification";
 import { getVersion } from "./tools/version";
 
 
-async function main() {
+type DeploymentAction = (safeTransactions: string[], abi: any, contractManager: ContractManager) => Promise<void>;
+
+export async function upgrade(
+    targetVersion: string,
+    contractNamesToUpgrade: string[],
+    deployNewContracts: DeploymentAction,
+    initialize: DeploymentAction)
+{
     if (!process.env.ABI) {
         console.log(chalk.red("Set path to file with ABI and addresses to ABI environment variables"));
         return;
@@ -41,7 +47,7 @@ async function main() {
     };
     const version = await getVersion();
     if (deployedVersion) {
-        if (deployedVersion !== "1.7.2-stable.0") {
+        if (deployedVersion !== targetVersion) {
             console.log(chalk.red(`This script can't upgrade version ${deployedVersion} to ${version}`));
             process.exit(1);
         }
@@ -69,34 +75,28 @@ async function main() {
         safe = safeMock.address;
         await (await proxyAdmin.transferOwnership(safe)).wait();
         await (await contractManager.transferOwnership(safe)).wait();
+        for (const contractName of
+            ["SkaleToken"].concat(contractNamesToUpgrade
+                .filter(name => !['TimeHelpers', 'Decryption', 'ECDH', 'Wallets'].includes(name)))) {
+                    const contractFactory = await getContractFactory(contractName);
+                    let _contract = contractName;
+                    if (contractName === "BountyV2") {
+                        if (!abi[getContractKeyInAbiFile(contractName) + "_address"])
+                        _contract = "Bounty";
+                    }
+                    const contractAddress = abi[getContractKeyInAbiFile(_contract) + "_address"];
+                    const contract = contractFactory.attach(contractAddress);
+                    console.log(chalk.blue(`Grant access to ${contractName}`));
+                    await (await contract.grantRole(await contract.DEFAULT_ADMIN_ROLE(), safe)).wait();
+        }
     }
 
-    // Deploy Wallets
-    const walletsName = "Wallets";
-    console.log(chalk.green("Deploy", walletsName));
-    const walletsFactory = await ethers.getContractFactory(walletsName);
-    const wallets = (await upgrades.deployProxy(walletsFactory, [contractManager.address])) as Wallets;
-    await wallets.deployTransaction.wait();
-    console.log(chalk.green("Transfer ownership"));
-    await (await wallets.grantRole(await wallets.DEFAULT_ADMIN_ROLE(), safe)).wait();
-    await (await wallets.revokeRole(await wallets.DEFAULT_ADMIN_ROLE(), deployer.address)).wait();
-    console.log(chalk.yellowBright("Prepare transaction to register", walletsName));
-    safeTransactions.push(encodeTransaction(
-        0,
-        contractManager.address,
-        0,
-        contractManager.interface.encodeFunctionData("setContractsAddress", [walletsName, wallets.address])
-    ));
-    abi[getContractKeyInAbiFile(walletsName) + "_address"] = wallets.address;
-    abi[getContractKeyInAbiFile(walletsName) + "_abi"] = getAbi(wallets.interface);
-    await verifyProxy(walletsName, wallets.address);
-
-    // remove Wallets from list
-    contracts.pop();
+    // Deploy new contracts
+    await deployNewContracts(safeTransactions, abi, contractManager);
 
     // deploy new implementations
     const contractsToUpgrade: {proxyAddress: string, implementationAddress: string, name: string, abi: any}[] = [];
-    for (const contract of ["ContractManager"].concat(contracts)) {
+    for (const contract of contractNamesToUpgrade) {
         const contractFactory = await getContractFactory(contract);
         let _contract = contract;
         if (contract === "BountyV2") {
@@ -133,72 +133,7 @@ async function main() {
         abi[getContractKeyInAbiFile(contract.name) + "_abi"] = contract.abi;
     }
 
-    // Initialize SegmentTree in Nodes
-    const nodesName = "Nodes";
-    const nodesContractFactory = await getContractFactory(nodesName);
-    const nodesAddress = abi[getContractKeyInAbiFile(nodesName) + "_address"];
-    if (nodesAddress) {
-        console.log(chalk.yellowBright("Prepare transaction to initialize", nodesName));
-        const nodes = (nodesContractFactory.attach(nodesAddress)) as Nodes;
-        if (safeMock) {
-            console.log(chalk.blue("Grant access to initialize nodes"));
-            await (await nodes.grantRole(await nodes.DEFAULT_ADMIN_ROLE(), safe)).wait();
-        }
-        safeTransactions.push(encodeTransaction(
-            0,
-            nodes.address,
-            0,
-            nodes.interface.encodeFunctionData("initializeSegmentTreeAndInvisibleNodes")
-        ));
-    } else {
-        console.log(chalk.red("Nodes address was not found!"));
-        console.log(chalk.red("Check your abi!"));
-        process.exit(1);
-    }
-
-    // Initialize schain types
-    const schainsInternalName = "SchainsInternal";
-    const schainsInternalFactory = await getContractFactory(schainsInternalName);
-    const schainsInternalAddress = abi[getContractKeyInAbiFile(schainsInternalName) + "_address"];
-    if (schainsInternalAddress) {
-        console.log(chalk.yellowBright("Prepare transactions to initialize schains types"));
-        const schainsInternal = (schainsInternalFactory.attach(schainsInternalAddress)) as SchainsInternal;
-        if (safeMock) {
-            console.log(chalk.blue("Grant access to initialize schains types"));
-            await (await schainsInternal.grantRole(await schainsInternal.DEFAULT_ADMIN_ROLE(), safe)).wait();
-        }
-        console.log(chalk.yellowBright("Number of Schain types will be set to 0"));
-        safeTransactions.push(encodeTransaction(
-            0,
-            schainsInternal.address,
-            0,
-            schainsInternal.interface.encodeFunctionData("setNumberOfSchainTypes", [0]),
-        ));
-
-        console.log(chalk.yellowBright("Schain Type Small will be added"));
-        safeTransactions.push(encodeTransaction(
-            0,
-            schainsInternal.address,
-            0,
-            schainsInternal.interface.encodeFunctionData("addSchainType", [1, 16]),
-        ));
-
-        console.log(chalk.yellowBright("Schain Type Medium will be added"));
-        safeTransactions.push(encodeTransaction(
-            0,
-            schainsInternal.address,
-            0,
-            schainsInternal.interface.encodeFunctionData("addSchainType", [4, 16]),
-        ));
-
-        console.log(chalk.yellowBright("Schain Type Large will be added"));
-        safeTransactions.push(encodeTransaction(
-            0,
-            schainsInternal.address,
-            0,
-            schainsInternal.interface.encodeFunctionData("addSchainType", [128, 16]),
-        ));
-    }
+    await initialize(safeTransactions, abi, contractManager);
 
     // write version
     if (safeMock) {
@@ -249,6 +184,101 @@ async function main() {
     await fs.writeFile(`data/skale-manager-${version}-${network.name}-abi.json`, JSON.stringify(abi, null, 4));
 
     console.log("Done");
+}
+
+
+async function main() {
+    // remove Wallets from list
+    contracts.pop();
+
+    await upgrade(
+        "1.7.2-stable.0",
+        ["ContractManager"].concat(contracts),
+        async (safeTransactions, abi, contractManager) => {
+            const safe = await contractManager.owner();
+            const [ deployer ] = await ethers.getSigners();
+
+            // Deploy Wallets
+            const walletsName = "Wallets";
+            console.log(chalk.green("Deploy", walletsName));
+            const walletsFactory = await ethers.getContractFactory(walletsName);
+            const wallets = (await upgrades.deployProxy(walletsFactory, [contractManager.address])) as Wallets;
+            await wallets.deployTransaction.wait();
+            console.log(chalk.green("Transfer ownership"));
+            await (await wallets.grantRole(await wallets.DEFAULT_ADMIN_ROLE(), safe)).wait();
+            await (await wallets.revokeRole(await wallets.DEFAULT_ADMIN_ROLE(), deployer.address)).wait();
+            console.log(chalk.yellowBright("Prepare transaction to register", walletsName));
+            safeTransactions.push(encodeTransaction(
+                0,
+                contractManager.address,
+                0,
+                contractManager.interface.encodeFunctionData("setContractsAddress", [walletsName, wallets.address])
+            ));
+            abi[getContractKeyInAbiFile(walletsName) + "_address"] = wallets.address;
+            abi[getContractKeyInAbiFile(walletsName) + "_abi"] = getAbi(wallets.interface);
+            await verifyProxy(walletsName, wallets.address);
+        },
+        async (safeTransactions, abi) => {
+
+            // Initialize SegmentTree in Nodes
+            const nodesName = "Nodes";
+            const nodesContractFactory = await getContractFactory(nodesName);
+            const nodesAddress = abi[getContractKeyInAbiFile(nodesName) + "_address"];
+            if (nodesAddress) {
+                console.log(chalk.yellowBright("Prepare transaction to initialize", nodesName));
+                const nodes = (nodesContractFactory.attach(nodesAddress)) as Nodes;
+                safeTransactions.push(encodeTransaction(
+                    0,
+                    nodes.address,
+                    0,
+                    nodes.interface.encodeFunctionData("initializeSegmentTreeAndInvisibleNodes")
+                ));
+            } else {
+                console.log(chalk.red("Nodes address was not found!"));
+                console.log(chalk.red("Check your abi!"));
+                process.exit(1);
+            }
+
+            // Initialize schain types
+            const schainsInternalName = "SchainsInternal";
+            const schainsInternalFactory = await getContractFactory(schainsInternalName);
+            const schainsInternalAddress = abi[getContractKeyInAbiFile(schainsInternalName) + "_address"];
+            if (schainsInternalAddress) {
+                console.log(chalk.yellowBright("Prepare transactions to initialize schains types"));
+                const schainsInternal = (schainsInternalFactory.attach(schainsInternalAddress)) as SchainsInternal;
+                console.log(chalk.yellowBright("Number of Schain types will be set to 0"));
+                safeTransactions.push(encodeTransaction(
+                    0,
+                    schainsInternal.address,
+                    0,
+                    schainsInternal.interface.encodeFunctionData("setNumberOfSchainTypes", [0]),
+                ));
+
+                console.log(chalk.yellowBright("Schain Type Small will be added"));
+                safeTransactions.push(encodeTransaction(
+                    0,
+                    schainsInternal.address,
+                    0,
+                    schainsInternal.interface.encodeFunctionData("addSchainType", [1, 16]),
+                ));
+
+                console.log(chalk.yellowBright("Schain Type Medium will be added"));
+                safeTransactions.push(encodeTransaction(
+                    0,
+                    schainsInternal.address,
+                    0,
+                    schainsInternal.interface.encodeFunctionData("addSchainType", [4, 16]),
+                ));
+
+                console.log(chalk.yellowBright("Schain Type Large will be added"));
+                safeTransactions.push(encodeTransaction(
+                    0,
+                    schainsInternal.address,
+                    0,
+                    schainsInternal.interface.encodeFunctionData("addSchainType", [128, 16]),
+                ));
+            }
+        });
 }
 
 if (require.main === module) {
