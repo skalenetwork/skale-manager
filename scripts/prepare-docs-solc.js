ead1a96ab5c834cb82247f65bba8062e07277dcd
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-08-03 11:08:54 +0300

    Hardcode solc version

--- scripts/prepare-docs-solc.js
+++ scripts/prepare-docs-solc.js
@@ -1,12 +1,10 @@
-const hre = require('hardhat');
-
 const { getCompilersDir } = require('hardhat/internal/util/global-dir');
 const { CompilerDownloader } = require('hardhat/internal/solidity/compiler/downloader');
 const { Compiler } = require('hardhat/internal/solidity/compiler');
 
-const [{ version }] = hre.config.solidity.compilers;
+const version = "0.6.10";
 
-async function getSolc () {
+async function getSolc () {  
   const downloader = new CompilerDownloader(await getCompilersDir(), { forceSolcJs: true });
   const { compilerPath } = await downloader.getDownloadedCompilerPath(version);
   const compiler = new Compiler(compilerPath);
