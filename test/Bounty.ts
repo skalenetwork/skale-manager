7520a3e50c5d71543b649b45843ae1d5dc6dc281
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-11-23 15:40:17 +0200

    SKALE-4695 Remove web3 from bounty test

--- test/Bounty.ts
+++ test/Bounty.ts
@@ -3,7 +3,6 @@ import {
     ConstantsHolder,
     BountyV2,
     NodesMock,
-    SkaleManager,
     DelegationController,
     SkaleToken,
     ValidatorService
@@ -21,14 +20,12 @@ import { deploySkaleToken } from "./tools/deploy/skaleToken";
 import { deployDelegationController } from "./tools/deploy/delegation/delegationController";
 import { deployValidatorService } from "./tools/deploy/delegation/validatorService";
 import { deployDelegationPeriodManager } from "./tools/deploy/delegation/delegationPeriodManager";
-import { deployDistributor } from "./tools/deploy/delegation/distributor";
 import { deploySkaleManagerMock } from "./tools/deploy/test/skaleManagerMock";
-import { privateKeys } from "./tools/private-keys";
 import * as elliptic from "elliptic";
-import { ethers, web3 } from "hardhat";
+import { ethers } from "hardhat";
 import { solidity } from "ethereum-waffle";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import { BigNumber, Event, ContractFactory } from "ethers";
+import { BigNumber, Event } from "ethers";
 import { deployPunisher } from "./tools/deploy/delegation/punisher";
 
 import { deployLibraries, getLinkedContractFactory } from "../test/tools/deploy/factory";
@@ -741,62 +738,5 @@ describe("Bounty", () => {
                 totalBounty += bounty;
             });
         });
-
-        // this test was used to manually check bounty distribution
-
-        // it("30 nodes by 1 each day", async () => {
-        //     const nodesCount = 30;
-        //     const result = new Map<number, object[]>();
-        //     const queue = []
-        //     for (let i = 0; i < nodesCount; ++i) {
-        //         await nodes.registerNodes(1, validatorId);
-        //         console.log("Node", i, "is registered", new Date(await currentTime(ethers) * 1000))
-        //         await skipTime(ethers, day);
-        //         result.set(i, []);
-        //         queue.push({nodeId: i, getBountyTimestamp: (await bountyContract.getNextRewardTimestamp(i)).toNumber()})
-        //     }
-        //     let minBounty = Infinity;
-        //     let maxBounty = 0;
-        //     const startTime = await currentTime(ethers);
-        //     queue.sort((a, b) => {
-        //         return b.getBountyTimestamp - a.getBountyTimestamp;
-        //     });
-        //     for (let timestamp = startTime; timestamp < startTime + 365 * day; timestamp = await currentTime(ethers)) {
-        //         const nodeInfo: {nodeId: number, getBountyTimestamp: number} | undefined = queue.pop();
-        //         assert(nodeInfo !== undefined);
-        //         if (nodeInfo) {
-        //             const nodeId = nodeInfo.nodeId;
-        //             if (timestamp < nodeInfo.getBountyTimestamp) {
-        //                 await skipTime(ethers, nodeInfo.getBountyTimestamp - timestamp);
-        //                 timestamp = await currentTime(ethers)
-        //             }
-        //             console.log("Node", nodeId, new Date(await currentTime(ethers) * 1000))
-        //             const bounty = web3.utils.toBN((await bountyContract.calculateBounty.call(nodeId))).div(ten18).toNumber();
-        //             // total[nodeIndex] += bounty;
-        //             await bountyContract.calculateBounty(nodeId);
-        //             await nodes.changeNodeLastRewardDate(nodeId);
-
-        //             nodeInfo.getBountyTimestamp = (await bountyContract.getNextRewardTimestamp(nodeId)).toNumber();
-        //             queue.push(nodeInfo)
-        //             queue.sort((a, b) => {
-        //                 return b.getBountyTimestamp - a.getBountyTimestamp;
-        //             });
-
-        //             minBounty = Math.min(minBounty, bounty);
-        //             maxBounty = Math.max(maxBounty, bounty);
-        //             result.get(nodeId)?.push({timestamp, bounty});
-        //         } else {
-        //             assert(false, "Internal error");
-        //         }
-        //     }
-        //     console.log(minBounty, maxBounty);
-        //     console.log(JSON.stringify(Array.from(result)));
-        //     const epochs = []
-        //     const timeHelpers = await deployTimeHelpers(contractManager);
-        //     for (let i = 0; i < 30; ++i) {
-        //         epochs.push((await timeHelpers.monthToTimestamp(i)).toNumber())
-        //     }
-        //     console.log(JSON.stringify(Array.from(epochs)));
-        // })
     });
 });
