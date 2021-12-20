0331821818c4586bf77973fedde7f9b208c3fae8
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-12-20 19:36:11 +0200

    Set NVM_DIR

--- scripts/test_upgrade.sh
+++ scripts/test_upgrade.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+export NVM_DIR=~/.nvm;
+source $NVM_DIR/nvm.sh;
+
 DEPLOYED_TAG=$(cat $GITHUB_WORKSPACE/DEPLOYED)
 DEPLOYED_VERSION=$(echo $DEPLOYED_TAG | cut -d '-' -f 1)
 DEPLOYED_DIR=$GITHUB_WORKSPACE/deployed-skale-manager/
@@ -11,8 +14,6 @@ CURRENT_NODE_VERSION=$(nvm current)
 
 git clone --branch $DEPLOYED_TAG https://github.com/$GITHUB_REPOSITORY.git $DEPLOYED_DIR
 cd $DEPLOYED_DIR
-export NVM_DIR=~/.nvm;
-source $NVM_DIR/nvm.sh;
 nvm install $DEPLOYED_WITH_NODE_VERSION
 nvm use $DEPLOYED_WITH_NODE_VERSION
 yarn install
