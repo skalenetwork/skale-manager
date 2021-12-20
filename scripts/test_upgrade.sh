6d930f5af75a079050dbb32fd156b433769eb7bf
Dmytro Stebaiev <dmytro@skalelabs.com>
2021-12-20 15:53:16 +0200

    Modify upgrade check script

--- scripts/test_upgrade.sh
+++ scripts/test_upgrade.sh
@@ -6,27 +6,26 @@ DEPLOYED_TAG=$(cat $GITHUB_WORKSPACE/DEPLOYED)
 DEPLOYED_VERSION=$(echo $DEPLOYED_TAG | cut -d '-' -f 1)
 DEPLOYED_DIR=$GITHUB_WORKSPACE/deployed-skale-manager/
 
-# TODO: remove when upgrade skale-manager with using new node js
-# or install old version of node
-if [[ $DEPLOYED_VERSION == "1.8.1" ]] && [[ $NODE_VERSION != 12.* ]]
-then
-    echo "Skip upgrade check because of incompatible node.js version"
-    exit 0
-fi
+DEPLOYED_WITH_NODE_VERSION="lts/erbium"
+CURRENT_NODE_VERSION=$(nvm current)
 
 git clone --branch $DEPLOYED_TAG https://github.com/$GITHUB_REPOSITORY.git $DEPLOYED_DIR
+cd $DEPLOYED_DIR
+nvm install $DEPLOYED_WITH_NODE_VERSION
+nvm use $DEPLOYED_WITH_NODE_VERSION
+yarn install
 
 npx ganache-cli --gasLimit 8000000 --quiet &
 GANACHE_PID=$!
 
-cd $DEPLOYED_DIR
-yarn install
 PRODUCTION=true VERSION=$DEPLOYED_VERSION npx hardhat run migrations/deploy.ts --network localhost
 rm $GITHUB_WORKSPACE/.openzeppelin/unknown-*.json || true
 cp .openzeppelin/unknown-*.json $GITHUB_WORKSPACE/.openzeppelin
 ABI_FILENAME="skale-manager-$DEPLOYED_VERSION-localhost-abi.json"
 cp "data/$ABI_FILENAME" "$GITHUB_WORKSPACE/data"
+
 cd $GITHUB_WORKSPACE
+nvm use $CURRENT_NODE_VERSION
 rm -r --interactive=never $DEPLOYED_DIR
 
 ABI="data/$ABI_FILENAME" npx hardhat run migrations/upgrade.ts --network localhost
