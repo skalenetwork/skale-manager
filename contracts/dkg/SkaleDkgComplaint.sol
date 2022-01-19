c733ca481286d212fa09e963e82f89b6cbd2b7f6
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-01-19 15:29:53 +0200

    SKALE-4886 Update solc and node

--- contracts/dkg/SkaleDkgComplaint.sol
+++ contracts/dkg/SkaleDkgComplaint.sol
@@ -21,7 +21,7 @@
     along with SKALE Manager.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-pragma solidity 0.8.9;
+pragma solidity 0.8.11;
 
 import "../SkaleDKG.sol";
 import "../ConstantsHolder.sol";
