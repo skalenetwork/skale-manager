ff468594cd72775c1d1e8f05b9467116fb567d7e
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-01-28 12:40:30 +0200

    Adjust DKG constants

--- contracts/ConstantsHolder.sol
+++ contracts/ConstantsHolder.sol
@@ -79,9 +79,9 @@ contract ConstantsHolder is Permissions, IConstantsHolder {
     uint public constant BOUNTY_LOCKUP_MONTHS = 2;
 
     uint public constant ALRIGHT_DELTA = 130000;
-    uint public constant BROADCAST_DELTA = 143000;
+    uint public constant BROADCAST_DELTA = 177478;
     uint public constant COMPLAINT_BAD_DATA_DELTA = 80995;
-    uint public constant PRE_RESPONSE_DELTA = 88903;
+    uint public constant PRE_RESPONSE_DELTA = 100061;
     uint public constant COMPLAINT_DELTA = 104611;
     uint public constant RESPONSE_DELTA = 49132;
 
