fbd11d7d44c082661e9af03863e0749b84f047ab
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-02-10 17:32:59 +0200

    SKALE-4964 Add upgrade-tools

--- migrations/upgrade-from-1.8.0-beta.1.ts
+++ migrations/upgrade-from-1.8.0-beta.1.ts
@@ -1,10 +1,15 @@
 import { contracts } from "./deploy";
-import { upgrade } from "./upgrade";
+import { upgrade } from "@skalenetwork/upgrade-tools";
+import { getDeployedVersion, setNewVersion } from "./upgrade";
 
 async function main() {
     await upgrade(
+        "skale-manager",
         "1.8.0-beta.1",
+        getDeployedVersion,
+        setNewVersion,
         ["ContractManager"].concat(contracts),
+        ["SkaleManager"],
         () => Promise.resolve(undefined),
         () => Promise.resolve(undefined)
     );
