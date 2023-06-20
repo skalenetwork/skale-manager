dd4063167e6f32e70166963c666d077a948d70b1
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-06-20 19:11:47 +0300

    Add ABI generation

---
+++ scripts/generateAbi.ts
@@ -0,0 +1,50 @@
+import { promises as fs } from 'fs';
+import { contracts } from "../migrations/deploy";
+import { ethers } from "hardhat";
+import { getAbi, getVersion } from '@skalenetwork/upgrade-tools';
+import { ContractFactory } from 'ethers';
+import { Libraries } from '@nomiclabs/hardhat-ethers/types';
+
+async function main() {
+    const allContracts = contracts.concat(["ContractManager", "TimeHelpersWithDebug"])
+    const abi: {[name: string]: []} = {};
+    const librariesRequirements: {[name: string]: string[]} = {
+        "Nodes": [
+            "SegmentTree"
+        ],
+        "SkaleDKG": [
+            "SkaleDkgAlright",
+            "SkaleDkgBroadcast",
+            "SkaleDkgComplaint",
+            "SkaleDkgPreResponse",
+            "SkaleDkgResponse"
+        ]
+    }
+    for (const contractName of allContracts) {
+        console.log(`Load ABI of ${contractName}`);
+        let factory: ContractFactory;
+        if (Object.keys(librariesRequirements).includes(contractName)) {
+            const libraries: Libraries = {};
+            for(const library of librariesRequirements[contractName]) {
+                libraries[library] = ethers.constants.AddressZero;
+            }
+            factory = await ethers.getContractFactory(contractName, {libraries});
+        } else {
+            factory = await ethers.getContractFactory(contractName);
+        }
+        abi[contractName] = getAbi(factory.interface);
+    }
+    const version = await getVersion();
+    const filename = `data/skale-manager-${version}-abi.json`;
+    console.log(`Save to ${filename}`)
+    await fs.writeFile(filename, JSON.stringify(abi, null, 4));
+}
+
+if (require.main === module) {
+    main()
+        .then(() => process.exit(0))
+        .catch(error => {
+            console.error(error);
+            process.exit(1);
+        });
+}
