08252d8ca3e62b73e0260d874bd79a5313ac6f34
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-09-26 16:10:39 +0300

    Set block time during deployment testing

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-GANACHE_SESSION=$(npx ganache --😈 --miner.blockGasLimit 8000000)
+# Have to set --miner.blockTime 1
+# because there is a bug in ganache
+# https://github.com/trufflesuite/ganache/issues/4165
+# TODO: remove --miner.blockTime 1
+# when ganache processes pending queue correctly
+# to speed up testing process
+GANACHE_SESSION=$(npx ganache --😈 --miner.blockGasLimit 8000000 --miner.blockTime 1)
 
 PRODUCTION=true npx hardhat run migrations/deploy.ts --network localhost
 
