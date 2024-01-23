43d5ccf1c568b1670fe108474939074dda80773a
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-01-23 16:34:57 +0200

    Update response gas constant

--- contracts/ConstantsHolder.sol
+++ contracts/ConstantsHolder.sol
@@ -81,9 +81,9 @@ contract ConstantsHolder is Permissions, IConstantsHolder {
     uint256 public constant ALRIGHT_DELTA = 134161;
     uint256 public constant BROADCAST_DELTA = 177490;
     uint256 public constant COMPLAINT_BAD_DATA_DELTA = 80995;
-    uint256 public constant PRE_RESPONSE_DELTA = 100061;
+    uint256 public constant PRE_RESPONSE_DELTA = 114511;
     uint256 public constant COMPLAINT_DELTA = 203048;
-    uint256 public constant RESPONSE_DELTA = 40312;
+    uint256 public constant RESPONSE_DELTA = 54762;
 
     // MSR - Minimum staking requirement
     uint256 public msr;
