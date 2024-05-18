ed7153038fbcc864b7ee8cbae640be1b73cf1ba9
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-05-18 10:43:06 +0300

    Add changeLifetime function

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -78,7 +78,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer:{
             enabled: true,
-            runs: 200
+            runs: 100
           }
         }
       }
