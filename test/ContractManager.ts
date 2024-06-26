78fe7bed1733e2324640a46fc71e35747835759f
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-06-26 17:45:45 +0300

    Fix contract manager

--- test/ContractManager.ts
+++ test/ContractManager.ts
@@ -26,10 +26,10 @@ describe("ContractManager", () => {
 
   it("Should add a right contract address (ConstantsHolder) to the register", async () => {
     const simpleContractName = "Constants";
-    await contractManager.connect(user).setContractsAddress(simpleContractName, constantsHolder.address)
+    await contractManager.connect(user).setContractsAddress(simpleContractName, constantsHolder)
       .should.be.eventually.rejectedWith("Ownable: caller is not the owner");
-    await contractManager.setContractsAddress(simpleContractName, constantsHolder.address);
+    await contractManager.setContractsAddress(simpleContractName, constantsHolder);
 
-    (await contractManager.getContract("ConstantsHolder")).should.be.equal(constantsHolder.address);
+    (await contractManager.getContract("ConstantsHolder")).should.be.equal(constantsHolder);
   });
 });
