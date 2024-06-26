09a9a204e41606e3ef7e60e1a87e11095211c518
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-06-26 17:15:43 +0300

    Fix generateAbi

--- scripts/generateAbi.ts
+++ scripts/generateAbi.ts
@@ -3,7 +3,7 @@ import {contracts} from "../migrations/deploy";
 import {ethers} from "hardhat";
 import {getAbi, getVersion} from '@skalenetwork/upgrade-tools';
 import {ContractFactory} from 'ethers';
-import {Libraries} from '@nomiclabs/hardhat-ethers/types';
+import {Libraries} from '@nomicfoundation/hardhat-ethers/types'
 
 async function main() {
     const allContracts = contracts.concat(["SkaleToken", "TimeHelpersWithDebug"])
