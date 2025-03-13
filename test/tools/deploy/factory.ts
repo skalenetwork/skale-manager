663cfd94e025a82cb4da911d58f7c599435b74b8
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2025-03-13 11:14:13 +0000

    bump version of upgrade-tools that contains update for test/tools/factory.ts

--- test/tools/deploy/factory.ts
+++ test/tools/deploy/factory.ts
@@ -1,8 +1,7 @@
 import {ethers, upgrades} from "hardhat";
 import {ContractManager} from "../../../typechain-types";
 import {AddressLike, Contract} from "ethers";
-import {NonceProvider} from "@skalenetwork/upgrade-tools/dist/src/nonceProvider";
-
+import {deployLibrariesSequential} from "@skalenetwork/upgrade-tools";
 async function defaultDeploy<ContractType = Contract>(contractName: string,
                              contractManager: ContractManager) {
     const contractFactory = await ethers.getContractFactory(contractName);
@@ -70,7 +69,7 @@ function deployWithLibraryFunctionFactory<ContractType extends AddressLike = Con
         = () => Promise.resolve(undefined)
 ) {
     return async (contractManager: ContractManager) => {
-        const libraries = await deployLibrariesSequentially(libraryNames);
+        const libraries = await deployLibrariesSequential(libraryNames);
         const contractFactory = await ethers.getContractFactory(contractName, {libraries: Object.fromEntries(libraries)});
         try {
             return contractFactory.attach(await contractManager.getContract(contractName)) as unknown as ContractType;
@@ -94,7 +93,7 @@ function deployWithLibraryWithConstructor<ContractType extends AddressLike = Con
         = () => Promise.resolve(undefined)
 ) {
     return async (contractManager: ContractManager) => {
-        const libraries = await deployLibrariesSequentially(libraryNames);
+        const libraries = await deployLibrariesSequential(libraryNames);
         const contractFactory = await ethers.getContractFactory(contractName, {libraries: Object.fromEntries(libraries)});
         try {
             return contractFactory.attach(await contractManager.getContract(contractName)) as unknown as ContractType;
@@ -105,30 +104,6 @@ function deployWithLibraryWithConstructor<ContractType extends AddressLike = Con
             return instance;
         }
     }
-}
-
-const deployLibrary = async (
-    libraryName: string,
-    nonceProvider: NonceProvider
-) => {
-    const Library = await ethers.getContractFactory(libraryName);
-    const library = await Library.
-        deploy({"nonce": nonceProvider.reserveNonce()});
-    await library.waitForDeployment()
-    return await library.getAddress();
-};
-
-async function deployLibrariesSequentially(libraryNames: string[]){
-    const [deployer] = await ethers.getSigners();
-    const initializedNonceProvider = await NonceProvider.createForWallet(deployer);
-    const libraries = new Map<string, string>();
-
-    for (const lib of libraryNames){
-        // Parallelization can't occur in testing environment (no mempool for transactions)
-        // eslint-disable-next-line no-await-in-loop
-        libraries.set(lib, await deployLibrary(lib, initializedNonceProvider));
-    }
-    return libraries;
 }
 
 export {
