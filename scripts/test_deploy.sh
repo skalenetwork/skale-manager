1decc14ae984a28609d6d03bdab589fa5defdfba
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2025-10-15 13:06:12 +0100

    test deploy to anvil

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -9,10 +9,22 @@ set -e
 # when ganache processes pending queue correctly
 # to speed up testing process
 
-echo "Deploy on ganache node"
-GANACHE_SESSION=$(npx ganache --😈 --miner.blockGasLimit 8000000 --miner.blockTime 1)
-PRODUCTION=true npx hardhat run migrations/deploy.ts --network localhost
-npx ganache instances stop $GANACHE_SESSION
+#echo "Deploy on ganache node"
+#GANACHE_SESSION=$(npx ganache --😈 --miner.blockGasLimit 8000000 --miner.blockTime 1)
+#PRODUCTION=true npx hardhat run migrations/deploy.ts --network localhost
+#npx ganache instances stop $GANACHE_SESSION
 
-echo "Deploy on hardhat node"
-PRODUCTION=true npx hardhat run migrations/deploy.ts
+#echo "Deploy on hardhat node"
+#PRODUCTION=true npx hardhat run migrations/deploy.ts
+
+
+echo "Deploy to anvil node"
+anvil > anvil.log 2>&1 &
+
+# Store the Process ID (PID)
+ANVIL_PID=$!
+
+# Give Anvil a moment to start up
+sleep 2
+PRODUCTION=true npx hardhat run migrations/deploy.ts --network localhost
+kill $ANVIL_PID
