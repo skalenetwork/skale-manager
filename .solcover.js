b88c5e67e623c52e4672f4f99b35dd8df49cc428
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2026-01-19 16:39:50 +0000

    add basic optimizer settings to coverage setup and tests

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,13 @@
 module.exports = {
-    skipFiles: ['thirdparty/', 'interfaces/', 'test/']
+    skipFiles: ['thirdparty/', 'interfaces/', 'test/'],
+    configureYulOptimizer: true,
+    // 2. Explicitly enable all standard optimization steps
+    solcOptimizerDetails: {
+        yul: true,
+        // Reduces slightly coverage time - not enough to justify losing coverage detail
+        //inliner: true,
+
+        // Common Subexpression Elimination (reduces coverage time by ~50% on heavy math calculations)
+        cse: true,  //
+    },
 };
