342ada106af69f3ea86b77270258d11b7a8ef283
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2026-01-21 15:50:42 +0000

    add gas calculation to migrations/ scripts

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -8,8 +8,34 @@ import {
     getContractFactory,
 } from '@skalenetwork/upgrade-tools';
 import {Contract, Interface, resolveAddress} from 'ethers';
-import {TransactionMinedTimeout} from "@openzeppelin/upgrades-core";
+import {isDevelopmentNetwork, TransactionMinedTimeout} from "@openzeppelin/upgrades-core";
 
+
+export async function isLocalNetwork() {
+    let result = false;
+    try {
+        result = await isDevelopmentNetwork(ethers.provider);
+    } catch {
+        console.error("Failed to detect network type, assuming non-development network");
+    }
+    return result;
+}
+
+export async function calculateGasSpent(startBlock: number, endBlock: number, user: string) {
+    let totalGasUsed = BigInt(0);
+    for (let blockNumber = startBlock; blockNumber <= endBlock; blockNumber++) {
+        const block = await ethers.provider.getBlock(blockNumber, true);
+        if (block === null) throw new Error(`Block ${blockNumber} not found`);
+        for (const txHash of block.transactions) {
+            const tx = await ethers.provider.getTransactionReceipt(txHash);
+            if (tx === null) throw new Error(`Transaction ${txHash} not found`);
+            if (tx.from.toLowerCase() === user.toLowerCase()) {
+                totalGasUsed += BigInt(tx.gasUsed);
+            }
+        }
+    }
+    return totalGasUsed;
+}
 
 function getInitializerParameters(contract: string, contractManagerAddress: string) {
     if (["TimeHelpers", "Decryption", "ECDH"].includes(contract)) {
@@ -72,7 +98,7 @@ async function main() {
     if (await ethers.provider.getCode("0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24") === "0x") {
         await run("erc1820");
     }
-
+    const startBlock = await ethers.provider.getBlockNumber();
     let production = false;
 
     if (process.env.PRODUCTION === "true") {
@@ -198,6 +224,13 @@ async function main() {
     }
 
     console.log("Done");
+
+    if (await isLocalNetwork()) {
+        console.log("Calculating gas used by deployer", owner.address);
+        const endBlock = await ethers.provider.getBlockNumber();
+        const gasUsed = await calculateGasSpent(startBlock, endBlock, owner.address);
+        console.log(`Gas used by deployer: ${gasUsed}`);
+    }
 }
 
 if (require.main === module) {
