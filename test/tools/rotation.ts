6f44b25e5e783726ee027aeb17a38057148dc2e4
Dmytro Stebaiev <dimastebaev@gmail.com>
2026-05-14 17:27:29 +0300

    Add rotation tools

---
+++ test/tools/rotation.ts
@@ -0,0 +1,12 @@
+import { NodeRotation, SchainsInternal } from "../../typechain-types"
+
+export const getBroadcastingNodes = async (schainHash: string, schainsInternal: SchainsInternal, nodeRotation: NodeRotation) => {
+    const nodes = await schainsInternal.getNodesInGroup(schainHash);
+    const broadcastingNodes = [];
+    for (const node of nodes) {
+        if (await nodeRotation.shouldSendBroadcast(schainHash, node)) {
+            broadcastingNodes.push(node);
+        }
+    }
+    return broadcastingNodes;
+}
