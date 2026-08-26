bca589430ecebe058a9b6cebb51c7be6a72309a0
Dmytro Stebaiev <dimastebaev@gmail.com>
2026-08-26 18:33:10 +0300

    Trigger CI

--- contracts/DKR.sol
+++ contracts/DKR.sol
@@ -133,7 +133,6 @@ interface IDkrNodeRotation is INodeRotation {
     function schainForDkr(DkrId dkrId) external view returns (bytes32 schainHash);
 }
 
-
 /**
  * @title DKR
  * @dev Contains functions to manage distributed key re-sharing.
